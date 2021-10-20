79fbd47316acba22b9cfa5af8da34508b8ccc540
fitrh <fitrahm17h@student.unhas.ac.id>
2021-10-20 11:54:34 +0800

    feat(conditional): add assignment 1.1, `Cashier.java`

---
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -0,0 +1,16 @@
+package ip.syssrc.conditional;
+
+/**
+ * Cashier
+ *
+ * Assignment 1.1
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *
+ */
+public class Cashier {
+
+    public static void main(String[] args) {
+
+    }
+}
