92bc0ad4f712da02b39347afb8243d57983b609a
erwinarif31 <muherwinarif31@gmail.com>
2021-10-20 23:42:31 +0800

    feat(condition/RoleAccess): add switch block
    To handle output

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -20,7 +20,7 @@ public class RoleAccess {
             System.out.println("For example, try 'Admin'.");
         } else if (args.length == 1) {
             // Your magic is here
-            switch (args[0]) {
+            switch (args[0]) { /* mengecek apakah role valid atau tidak */
                 case "Super Admin":
                     isValidRole = true;
                     break;
@@ -33,8 +33,30 @@ public class RoleAccess {
                 default:
                     break;  
             }
-            if ( ) {
+            
+            if (isValidRole) { 
                 // Another magic
+                switch (args[0]) { /* print output sesuai role */
+                    case "Super Admin":
+                        menuIndex++;
+                        subMenuIndex++;
+                        System.out.printf("%d. Super Admin\n", menuIndex);
+                        System.out.printf("%2d.%d. CRUD Super Admin\n", menuIndex, subMenuIndex);
+                        System .out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex++);
+                        System.out.printf("%2d.%d. CRUD User\n\n", menuIndex, subMenuIndex++);
+                    case "Admin":
+                        menuIndex++;
+                        subMenuIndex = 1;
+                        System.out.printf("%d. Admin\n", menuIndex);
+                        System.out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex);
+                        System.out.printf("%2d.%d. CRUD User\n\n", menuIndex, subMenuIndex++);
+                    default:
+                        menuIndex++;
+                        subMenuIndex = 1;
+                        System.out.printf("%d. User\n", menuIndex);
+                        System.out.printf("%2d.%d. View\n", menuIndex, subMenuIndex);
+                        System.out.printf("%2d.%d. Edit\n", menuIndex, subMenuIndex++);
+                }
             } else {
                 System.out.println("Invalid Role");
                 System.out.println("Valid Role : Super Admin, Admin, User");
