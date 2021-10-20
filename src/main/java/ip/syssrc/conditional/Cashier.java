613f312d25049a962492f040b3a4f5a47b456fb7
erwinarif31 <muherwinarif31@gmail.com>
2021-10-20 18:54:45 +0800

    feat(conditional/Cashier): add 'java.util.Scanner' oject and

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -1,16 +1,19 @@
 package ip.syssrc.conditional;
-
+import java.util.Scanner;
 /**
  * Cashier
  *
  * Assignment 1.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211059 - Muhammad Erwin Arif <muherwinarif31@gmail.com>
  *
  */
 public class Cashier {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int totalBelanja, totalBayar, totalKembalian;
+        
     }
 }
