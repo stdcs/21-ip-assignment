76399d16d19f28f702605a027c27e3ce490a4c68
fitrh <fitrahm17h@student.unhas.ac.id>
2021-10-20 11:56:32 +0800

    feat(conditional): add assignment 1.2, `RoleAccess.java`

---
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -0,0 +1,16 @@
+package ip.syssrc.conditional;
+
+/**
+ * RoleAccess
+ *
+ * Assignment 1.2
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *
+ */
+public class RoleAccess {
+
+    public static void main(String[] args) {
+
+    }
+}
