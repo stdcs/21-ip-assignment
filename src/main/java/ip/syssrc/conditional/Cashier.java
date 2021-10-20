492c885ff4a5456eddde00ae0754230e293902a0
Hafieddz <hasmy41@gmail.com>
2021-10-21 00:02:11 +0800

    feat(conditional/Cashier): add variable and scanner input to variable

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -16,6 +16,7 @@ public class Cashier {
         Scanner in = new Scanner(System.in);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         // output dan variabel //
         int total = 0;
         int bayar = 0;
@@ -101,11 +102,15 @@ public class Cashier {
         }
         in.close();
 =======
+=======
+        // output dan variabel //
+>>>>>>> 673760f (feat(conditional/Cashier): add variable and scanner input to variable)
         System.out.printf("Berapa total harga belanja anda ? " );
         int total = in.nextInt();
         System.out.printf("Berapa total uang yang anda bayarkan ? ");
         int bayar = in.nextInt(); 
         int kembalian = bayar - total;
+<<<<<<< HEAD
         
             // jika uang mencukupi //
         if (bayar > total){
@@ -182,3 +187,7 @@ public class Cashier {
 =======
         
 >>>>>>> 7fa6cda (feat(conditional/Cashier): add 'java.util.Scanner' object)
+=======
+
+
+>>>>>>> 673760f (feat(conditional/Cashier): add variable and scanner input to variable)
