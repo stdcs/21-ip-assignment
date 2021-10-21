1432ff11af8f926bd6f0640a1bcad91857f4f020
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-10-21 23:36:48 +0700

    feat(conditional/RoleAccess) : add 'switch (args[0]) {' object

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -19,5 +19,19 @@ public class RoleAccess {
             System.out.println("What role you want to see ?");
             System.out.println("For example, try 'Admin'.");
           } else if (args.length == 1) {
+
+            switch (args[0]) {
+                case "Super Admin":
+                    isValidRole = true;
+                    break;
+                case "Admin":
+                    isValidRole = true;
+                    break;
+                case "User":
+                    isValidRole = true;
+                    break;
+                default:
+                    isValidRole = false;
+                    break;
     }
 }
