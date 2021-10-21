0a0e68e48c9e190ff7d2b43c984edb2b4e07f1ce
stalyaardani <stalyaardaniaries@gmail.com>
2021-10-21 23:44:47 +0800

    feat (conditional/RoleAccess) : add 'int menuIndex = 0' object

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -5,12 +5,19 @@ package ip.syssrc.conditional;
  *
  * Assignment 1.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211061 - St. Alya Ardani Aries <stalyaardaniaries@gamil.com>
  *
  */
 public class RoleAccess {
 
     public static void main(String[] args) {
-
+        int menuIndex = 0;
+        int subMenuIndex = 0;
+        boolean isValidRole = false;
+    
+        if (args.length == 0) {
+          System.out.println("What role you want to see ?");
+          System.out.println("For example, try 'Admin'.");
+        } 
     }
 }
