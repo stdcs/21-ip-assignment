7b80c0c491e351f86bb28492cbd9e399b53c0c47
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-10-21 22:03:32 +0700

    feat(conditional/Cashier) : add 'import java.util.Scanner;' object

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -1,16 +1,22 @@
 package ip.syssrc.conditional;
-
+import java.util.Scanner; 
 /**
  * Cashier
  *
  * Assignment 1.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211079 - Dhiyaa Unnisa <unnisadhiyaa25@gmail.com>
  *
  */
 public class Cashier {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Berapa total harga belanja Anda? ");
+        int totalBelanja = in.nextInt();
+
+        System.out.print("Berapa total uang yang Anda bayarkan? ");
+        int pembayaran = in.nextInt();
     }
 }
