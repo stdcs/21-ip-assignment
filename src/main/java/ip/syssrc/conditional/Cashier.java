bb3b6d4c8f99f394b768ba723315c20d5cab6741
SyifaUrRahmi <sifasisi3@gmail.com>
2021-10-21 14:40:15 +0800

    feat(conditional/Chashier : intitialize totalHarga and uangYangdiBayarkan variable

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -5,12 +5,13 @@ package ip.syssrc.conditional;
  *
  * Assignment 1.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211066 - Syifa Ur Rahmi <sifasisi3@gmail.com>
  *
  */
 public class Cashier {
 
     public static void main(String[] args) {
-
-    }
+        Scanner in = new Scanner(System.in);
+        int totalHarga = 0;
+        int uangYangdiBayarkan = 0;
 }
