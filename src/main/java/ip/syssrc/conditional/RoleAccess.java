d6b64af7c93d8b7f7985a8b00d2dc75c8bf5683e
SyifaUrRahmi <sifasisi3@gmail.com>
2021-10-21 15:00:05 +0800

    feat(conditional/RoleAccess) : add java.util.Scanner

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -5,12 +5,13 @@ package ip.syssrc.conditional;
  *
  * Assignment 1.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211066 - Syifa Ur Rahmi <sifasisi3@gmail.com>
  *
  */
+import java.util.Scanner;
 public class RoleAccess {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
     }
 }
