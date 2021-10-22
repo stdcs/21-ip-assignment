782184e1dfe6133033a42748bd36bfbecdb7780a
sofyanox12 <gaero38@gmail.com>
2021-10-22 15:14:52 +0900

    Revisi Cashier.java (assignment 1.1)
    Penentuan variabel sudah diubah barisannya dan lebih deskiriptif, serta pada interface hanya akan menerima inputan dalam bentuk integer sesuai arahan kakak.

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -1,119 +1,139 @@
-// NAMA : MUHAMMAD SOFYAN DAUD PUJAS
-// NIM : H071211045
-
 package ip.syssrc.conditional;
+
+/**
+ * Cashier
+ *
+ * Assignment 1.1
+ *
+ * @author H071211045 - MUHAMMAD SOFYAN DAUD PUJAS <gaero38@gmail.com>
+ *
+ */
 
 import java.util.Scanner;
 public class Cashier {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Menentukan Variabel
-        int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0;
+        int seratusRibu = 0; 
+        int limaPuluhRibu = 0; 
+        int duaPuluhRibu = 0; 
+        int sepuluhRibu = 0; 
+        int limaRibu = 0; 
+        int duaRibu = 0; 
+        int seribu = 0; 
+        int limaRatus = 0; 
+        int duaRatus = 0; 
+        int seratus = 0;
 
-        // Interface
+        int totalBelanja = 0;
+        int pembayaran = 0;
+
+        // Interface sederhana
+        System.out.println("\n-----[WELCOME TO SYSMART CASHIER]-----");
         System.out.print("\nBerapa total harga belanja anda ? Rp.");
-        int totalBelanja = sc.nextInt();
         
+        while (!sc.hasNextInt()) {
+            System.out.flush();
+            System.out.println("\nMaaf, pastikan inputan anda adalah angka");
+            sc.next();
+            System.out.print("\nBerapa total harga belanja anda ? Rp.");
+        } totalBelanja = sc.nextInt();
+
         System.out.print("Berapa total uang anda bayarkan ? Rp.");
-        int pembayaran = sc.nextInt();
+        while (!sc.hasNextInt()) {
+            System.out.flush();
+            System.out.println("\nMaaf, pastikan inputan anda adalah angka");
+            sc.next();
+            System.out.print("\nBerapa total uang anda bayarkan ? Rp.");
+        }
+        pembayaran = sc.nextInt();
 
         // Kalkulasikan
         int kembalian = pembayaran - totalBelanja;
-        if (pembayaran < totalBelanja) { System.out.println("\nPembayaran tidak cukup\nMaaf, anda memiliki hutang sebesar" + " Rp." + (totalBelanja-pembayaran)+"\n"); } else {
+
+        if (pembayaran < totalBelanja) { 
+            System.out.println("\nPembayaran tidak cukup!\n");
+            System.out.print("Maaf, anda memiliki hutang sebesar Rp."); 
+            System.out.println((totalBelanja-pembayaran)+"\n"); 
+        } else {
         System.out.println("\nTotal kembalian anda = " + kembalian);
         System.out.println("Dengan dengan rincian sebagai berikut : \n");
 
+        // Seleksi Kondisi
         if (kembalian >= 100000) {
             kembalian = kembalian - 100000;
-            a = a + 1;
+            seratusRibu = seratusRibu + 1;
         }
         if (kembalian >= 50000) {
             kembalian = kembalian - 50000;
-            b = b + 1;
+            limaPuluhRibu = limaPuluhRibu + 1;
         }
         if (kembalian >= 20000) {
             kembalian = kembalian - 20000;
-            c = c + 1;
-        }
-        if (kembalian >= 20000) {
-            kembalian = kembalian - 20000;
-            c = c + 1;
+            duaPuluhRibu = duaPuluhRibu + 1;
         }
         if (kembalian >= 10000) {
             kembalian = kembalian - 10000;
-            d = d + 1;
+            sepuluhRibu = sepuluhRibu + 1;
         }
         if (kembalian >= 5000) {
             kembalian = kembalian - 5000;
-            e = e + 1;
+            limaRibu = limaRibu + 1;
         }   
         if (kembalian >= 2000) {
             kembalian = kembalian - 2000;
-            f = f + 1;
-        }
-        if (kembalian >= 2000) {
-            kembalian = kembalian - 2000;
-            f = f + 1;
+            duaRibu = duaRibu + 1;
         }
         if (kembalian >= 1000) {
             kembalian = kembalian - 1000;
-            g = g + 1;
+            seribu = seribu + 1;
         }
         if (kembalian >= 500) {
             kembalian = kembalian - 500;
-            h = h + 1;
+            limaRatus = limaRatus + 1;
         }
         if (kembalian >= 200) {
             kembalian = kembalian - 200;
-            i = i + 1;
-        }
-        if (kembalian >= 200) {
-            kembalian = kembalian - 200;
-            i = i + 1;
+            duaRatus = duaRatus + 1;
         }
         if (kembalian >= 100) {
             kembalian = kembalian - 100;
-            j = j + 1;
+            seratus = seratus + 1;
         }
-        if (a > 0) {
-            System.out.println(a + " lembar uang Rp.100000");
+        if (seratusRibu > 0) {
+            System.out.println(seratusRibu + " lembar uang Rp.100000");
         } 
-        if (b > 0) {
-            System.out.println(b + " lembar uang Rp.50000");
+        if (limaPuluhRibu > 0) {
+            System.out.println(limaPuluhRibu + " lembar uang Rp.50000");
         } 
-        if (c > 0) {
-            System.out.println(c + " lembar uang Rp.20000");
+        if (duaPuluhRibu > 0) {
+            System.out.println(duaPuluhRibu + " lembar uang Rp.20000");
         } 
-        if (d > 0) {
-            System.out.println(d + " lembar uang Rp.10000");
+        if (sepuluhRibu > 0) {
+            System.out.println(sepuluhRibu + " lembar uang Rp.10000");
         } 
-        if (e > 0) {
-            System.out.println(e + " lembar uang Rp.5000");
+        if (limaRibu > 0) {
+            System.out.println(limaRibu + " lembar uang Rp.5000");
         } 
-        if (f > 0) {
-            System.out.println(f + " lembar uang Rp.2000");
+        if (duaRibu > 0) {
+            System.out.println(duaRibu + " lembar uang Rp.2000");
         } 
-        if (g > 0) {
-            System.out.println(g + " lembar uang Rp.1000");
+        if (seribu > 0) {
+            System.out.println(seribu + " lembar uang Rp.1000");
         } 
-        if (h > 0) {
-            System.out.println(h + " lembar uang Rp.500");
+        if (limaRatus > 0) {
+            System.out.println(limaRatus + " lembar uang Rp.500");
         } 
-        if (i > 0) {
-            System.out.println(i + " lembar uang Rp.200");
+        if (duaRatus > 0) {
+            System.out.println(duaRatus + " lembar uang Rp.200");
         } 
-        if (j > 0) {
-            System.out.println(j + " lembar uang Rp.100");
+        if (seratus > 0) {
+            System.out.println(seratus + " lembar uang Rp.100");
         }
         if (kembalian > 0) {
             System.out.println("Dan sebanyak Rp. " + kembalian + " didonasikan");
         }
-        System.out.println();
-        System.out.println("\nLicensed to Sysmart, by sofyanox12\n");
-
     }
         sc.close();
     }
 }
-
-/* Licensed to SysMart, by sofyanox12 */
