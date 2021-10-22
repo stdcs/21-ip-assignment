c7ddb513b8df51ebc62f3948ded71ef75165da6e
FIRMANSYAH <frmnsyah33@gmail.com>
2021-10-22 21:21:32 +0800

    fix(conditional/RoleAccess): add mengurangi redundansi dan memperbaiki blok spasi

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -1,5 +1,4 @@
 package ip.syssrc.conditional;
-import java.util.*;
 
 /**
  * RoleAccess
@@ -12,31 +11,23 @@ import java.util.*;
 public class RoleAccess {
 
     public static void main(String[] args) {
-        int menuIndex = 0;
-    int subMenuIndex = 0;
-    boolean isValidRole = false;
+      int menuIndex = 0;
+      int subMenuIndex = 0;
+      boolean isValidRole = false;
 
-    if (args.length == 0){
+    if (args.length == 0) {
       System.out.println("What role you want to see ?");
       System.out.println("For example, try 'Admin'.");
-    }else if(args.length == 1){
-      switch(args[0]){
-        case "Super Admin":
-          isValidRole = true;
-          break;
-        case "Admin":
-          isValidRole = true;
-          break;
-        case "User":
-          isValidRole = true;
-          break;
-        default:
-        isValidRole = false;
-          break;
-      }
-      if(isValidRole == true){
-          switch (args[0] ){
-              case "Super Admin":
+      } else if (args.length == 1) {
+        switch (args[0]) {
+            case "Super Admin", "Admin", "User":
+              isValidRole = true; break;
+            default:
+              isValidRole = false; break;
+        } 
+        if (isValidRole == true) {
+          switch (args[0] ) {
+              case "Super Admin": 
                 menuIndex = menuIndex + 1;
                 System.out.printf("%d. Super Admin\n", menuIndex);
                 subMenuIndex = subMenuIndex + 1;
@@ -46,7 +37,7 @@ public class RoleAccess {
                 subMenuIndex = subMenuIndex + 1;
                 System.out.printf("%2d.%d. CRUD User \n", menuIndex, subMenuIndex);
                 System.out.println();
-              case "Admin":
+              case "Admin" :
                 menuIndex = menuIndex + 1;
                 subMenuIndex = 0;
                 System.out.printf("%d. Admin\n", menuIndex);
@@ -65,12 +56,12 @@ public class RoleAccess {
                 System.out.printf("%2d.%d. Edit", menuIndex, subMenuIndex);
               break;
           }
-      }else {
+      } else {
         System.out.println("Invalid Role");
         System.out.println("Valid Role : Super Admin, Admin, User");
-      }
-    }else{
+        }
+    } else {
       System.out.println("Too many argument");
-    }
+      }
     }
 }
