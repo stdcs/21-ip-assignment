fd963885680beab98804860c228a6f026c77c976
stalyaardani <stalyaardaniaries@gmail.com>
2021-10-23 12:56:10 +0800

    feat (conditional/Cashier) : change variable

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -36,17 +36,17 @@ public class Cashier {
             int kembalian = pembayaran - totalBelanja;
             System.out.println("Total kembalian Anda : " + kembalian);
             System.out.println("Dengan rincian sebagai berikut : ");
-            int a,b,c,d,e,f,g,h,i,j;
-            a = 0;
-            b = 0;
-            c = 0;
-            d = 0;
-            e = 0;
-            f = 0;
-            g = 0;
-            h = 0;
-            i = 0;
-            j = 0;
+            int seratusRibu, limaPuluhRibu, duaPuluhRibu, sepuluhRibu, limaRibu, duaRibu, seribu, limaRatusRupiah, duaRatusRupiah, seratusRupiah;
+            seratusRibu = 0;
+            limaPuluhRibu = 0;
+            duaPuluhRibu = 0;
+            sepuluhRibu = 0;
+            limaRibu = 0;
+            duaRibu = 0;
+            seribu = 0;
+            limaRatusRupiah = 0;
+            duaRatusRupiah = 0;
+            seratusRupiah = 0;
 
             if (kembalian >= 100000) {
                 kembalian = kembalian - 100000;
