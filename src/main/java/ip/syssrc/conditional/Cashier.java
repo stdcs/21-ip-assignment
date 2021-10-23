df75a429acadeb7f42d441585bc33698bb4e2113
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-10-23 16:08:20 +0700

    feat (conditional/Cashier) : add 'int kembalian = pembayaran - totalBelanja;'

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -36,100 +36,100 @@ public class Cashier {
             int kembalian = pembayaran - totalBelanja;
             System.out.println("Total kembalian Anda : " + kembalian);
             System.out.println("Dengan rincian sebagai berikut : ");
-            int a,b,c,d,e,f,g,h,i,j;
-            a = 0;
-            b = 0;
-            c = 0;
-            d = 0;
-            e = 0;
-            f = 0;
-            g = 0;
-            h = 0;
-            i = 0;
-            j = 0;
+            int seratusRibu, limaPuluhRibu, duaPuluhRibu, sepuluhRibu, limaRibu, duaRibu, seribu, limaRatusRupiah, duaRatusRupiah, seratusRupiah;
+            seratusRibu = 0;
+            limaPuluhRibu = 0;
+            duaPuluhRibu = 0;
+            sepuluhRibu = 0;
+            limaRibu = 0;
+            duaRibu = 0;
+            seribu = 0;
+            limaRatusRupiah = 0;
+            duaRatusRupiah = 0;
+            seratusRupiah = 0;
 
             if (kembalian >= 100000) {
                 kembalian = kembalian - 100000;
-                a = a + 1;
+                seratusRibu = seratusRibu + 1;
             }
             if (kembalian >= 50000) {
                 kembalian = kembalian - 50000;
-                b = b + 1;
+                limaPuluhRibu = limaPuluhRibu + 1;
             }
             if (kembalian >= 20000) {
                 kembalian = kembalian - 20000;
-                c = c + 1;
+                duaPuluhRibu = duaPuluhRibu + 1;
             }
             if (kembalian >= 20000) {
                 kembalian = kembalian - 20000;
-                c = c + 1;
+                duaPuluhRibu = duaPuluhRibu + 1;
             }
             if (kembalian >= 10000) {
                 kembalian = kembalian - 10000;
-                d = d + 1;
+                sepuluhRibu = sepuluhRibu + 1;
             }
             if (kembalian >= 5000) {
                 kembalian = kembalian - 5000;
-                e = e + 1;
+                limaRibu = limaRibu + 1;
             }
             if (kembalian >= 2000) {
                 kembalian = kembalian - 2000;
-                f = f + 1;
+                duaRibu = duaRibu + 1;
             }
             if (kembalian >= 2000) {
                 kembalian = kembalian - 2000;
-                f = f + 1;
+                duaRibu = duaRibu + 1;
             }
             if (kembalian >= 1000) {
                 kembalian = kembalian - 1000;
-                g = g + 1;
+                seribu = seribu + 1;
             }
             if (kembalian >= 500) {
                 kembalian = kembalian - 500;
-                h = h + 1;
+                limaRatusRupiah = limaRatusRupiah + 1;
             }
             if (kembalian >= 200) {
                 kembalian = kembalian - 200;
-                i = i + 1;
+                duaRatusRupiah = duaRatusRupiah + 1;
             }
             if (kembalian >= 200) {
                 kembalian = kembalian - 200;
-                i = i + 1;
+                duaRatusRupiah = duaRatusRupiah + 1;
             }
             if (kembalian >= 100) {
                 kembalian = kembalian - 100;
-                j = j + 1;
+                seratusRupiah = seratusRupiah + 1;
             }
 
-            if (a > 0) {
-                System.out.println(a + " lembar uang Rp. 100000");
+            if (seratusRibu > 0) {
+                System.out.println(seratusRibu + " lembar uang Rp. 100000");
             }
-            if (b > 0) {
-                System.out.println(b + " lembar uang Rp. 50000");
+            if (limaPuluhRibu > 0) {
+                System.out.println( limaPuluhRibu + " lembar uang Rp. 50000");
             }
-            if (c > 0) {
-                System.out.println(c + " lembar uang Rp. 20000");
+            if (duaPuluhRibu > 0) {
+                System.out.println(duaPuluhRibu + " lembar uang Rp. 20000");
             }
-            if (d > 0) {
-                System.out.println(d + " lembar uang Rp. 10000");
+            if (sepuluhRibu > 0) {
+                System.out.println(sepuluhRibu + " lembar uang Rp. 10000");
             }
-            if (e > 0) {
-                System.out.println(e + " lembar uang Rp. 5000");
+            if (limaRibu > 0) {
+                System.out.println(limaRibu + " lembar uang Rp. 5000");
             }
-            if (f > 0) {
-                System.out.println(f + " lembar uang Rp. 2000");
+            if (duaRibu > 0) {
+                System.out.println(duaRibu + " lembar uang Rp. 2000");
             }
-            if (g > 0) {
-                System.out.println(g + " lembar uang Rp. 1000");
+            if (seribu > 0) {
+                System.out.println(seribu + " lembar uang Rp. 1000");
             }
-            if (h > 0) {
-                System.out.println(h + " lembar uang Rp. 500");
+            if (limaRatusRupiah > 0) {
+                System.out.println(limaRatusRupiah + " lembar uang Rp. 500");
             }
-            if (i > 0) {
-                System.out.println(i + " lembar uang Rp. 200");
+            if (duaRatusRupiah > 0) {
+                System.out.println(duaRatusRupiah + " lembar uang Rp. 200");
             }
-            if (j > 0) {
-                System.out.println(j + " lembar uang Rp. 100");
+            if (seratusRupiah > 0) {
+                System.out.println(seratusRupiah + " lembar uang Rp. 100");
             }
             if (kembalian > 0) {
                 System.out.println("Dan sebanyak Rp. " + kembalian + " didonasikan");
