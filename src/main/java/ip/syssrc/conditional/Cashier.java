7e25693663ff61807bb94b51bfee2a6b5dffa762
dirganoob <92706538+dirganoob@users.noreply.github.com>
2021-10-24 21:36:28 +0800

    Update Cashier.java

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -8,117 +8,121 @@ package ip.syssrc.conditional;
  * @author H071121080- Dirgantry leonard Nugrah Boro <dirgantryleonardnugrahboro@gmail.com>
  *
  */
+import java.util.Scanner;
+
 public class Cashier {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int a, b;
+        try {
+            System.out.println("jumlahuang = ");
+            a = scan.nextInt();
 
-        System.out.println("jumlahuang = ");
-        int a = scan.nextInt();
+            System.out.println("harga = ");
+            b = scan.nextInt();
+            System.out.println("kembalian = ");
+            int c = a - b;
 
-        System.out.println("harga = ");
-        int b = scan.nextInt();
+            System.out.println(c);
 
-        System.out.println("kembalian = ");
-        int c = a - b;
+            int bil1;
 
-        System.out.println(c);
+            if (c < 50) {
+                System.out.println("Maaf, tidak ada nominal uang " + "sebesar " + c + " rupiah");
 
-        int bil1;
+            } else {
+                // besar 100000
+                if (c >= 100000) {
+                    bil1 = c / 100000;
+                    System.out.println(" uang Rp 100000 sebanyak " + bil1 + "lembar");
+                }
 
-        if (c < 50) {
-            System.out.println("Maaf, tidak ada nominal uang " + "sebesar " + c + " rupiah");
+                // antara 50000-100000
+                c = c % 100000;
+                if ((c >= 50000) && (c < 100000)) {
+                    bil1 = c / 50000;
+                    System.out.println("uang Rp 50000 sebanyak " + bil1 + "lembar");
+                }
 
-        } else {
-            // besar 100000
-            if (c >= 100000) {
-                bil1 = c / 100000;
-                System.out.println(" uang Rp 100000 sebanyak " + bil1 + "lembar");
+                // antara 20000-50000
+                c = c % 50000;
+                if ((c >= 20000) && (c < 50000)) {
+                    bil1 = c / 20000;
+                    System.out.println("uang Rp  20000 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 10000-20000
+                c = c % 20000;
+                if ((c >= 10000) && (c < 20000)) {
+                    bil1 = c / 10000;
+                    System.out.println(" uang Rp  10000 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 5000-10000
+                c = c % 10000;
+                if ((c >= 5000) && (c < 10000)) {
+                    bil1 = c / 5000;
+                    System.out.println(" uang Rp   5000 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 2000-5000
+                c = c % 5000;
+                if ((c >= 2000) && (c < 5000)) {
+                    bil1 = c / 2000;
+                    System.out.println(" uang Rp   2000 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 1000-2000
+                c = c % 2000;
+                if ((c >= 1000) && (c < 2000)) {
+                    bil1 = c / 1000;
+                    System.out.println("uang Rp   1000 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 500-1000
+                c = c % 1000;
+                if ((c >= 500) && (c < 1000)) {
+                    bil1 = c / 500;
+                    System.out.println(" uang Rp    500 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 200-500
+                c = c % 500;
+                if ((c >= 200) && (c < 500)) {
+                    bil1 = c / 200;
+                    System.out.println(" uang Rp    200 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 100-200
+                c = c % 200;
+                if ((c >= 100) && (c < 200)) {
+                    bil1 = c / 100;
+                    System.out.println(" uang Rp    100 sebanyak " + bil1 + "lembar");
+                }
+
+                // antara 50-100
+                c = c % 100;
+                if ((c >= 50) && (c < 100)) {
+                    bil1 = c / 50;
+                    System.out.println("uang Rp     50 sebanyak " + bil1 + "lembar");
+                }
+
+                // kurang 50
+                c = c % 50;
+                if ((c > 0) && (c < 50)) {
+                    bil1 = c;
+                    // satuan
+                    if (bil1 < 10)
+                        System.out.println("Rp      " + bil1 + " di donasikan");
+                    else if (bil1 >= 10)// puluhan
+                        System.out.println("Rp     " + bil1 + " di donasikan");
+                }
             }
-
-            // antara 50000-100000
-            c = c % 100000;
-            if ((c >= 50000) && (c < 100000)) {
-                bil1 = c / 50000;
-                System.out.println("uang Rp 50000 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 20000-50000
-            c = c % 50000;
-            if ((c >= 20000) && (c < 50000)) {
-                bil1 = c / 20000;
-                System.out.println("uang Rp  20000 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 10000-20000
-            c = c % 20000;
-            if ((c >= 10000) && (c < 20000)) {
-                bil1 = c / 10000;
-                System.out.println(" uang Rp  10000 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 5000-10000
-            c = c % 10000;
-            if ((c >= 5000) && (c < 10000)) {
-                bil1 = c / 5000;
-                System.out.println(" uang Rp   5000 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 2000-5000
-            c = c % 5000;
-            if ((c >= 2000) && (c < 5000)) {
-                bil1 = c / 2000;
-                System.out.println(" uang Rp   2000 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 1000-2000
-            c = c % 2000;
-            if ((c >= 1000) && (c < 2000)) {
-                bil1 = c / 1000;
-                System.out.println("uang Rp   1000 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 500-1000
-            c = c % 1000;
-            if ((c >= 500) && (c < 1000)) {
-                bil1 = c / 500;
-                System.out.println(" uang Rp    500 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 200-500
-            c = c % 500;
-            if ((c >= 200) && (c < 500)) {
-                bil1 = c / 200;
-                System.out.println(" uang Rp    200 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 100-200
-            c = c % 200;
-            if ((c >= 100) && (c < 200)) {
-                bil1 = c / 100;
-                System.out.println(" uang Rp    100 sebanyak " + bil1 + "lembar");
-            }
-
-            // antara 50-100
-            c = c % 100;
-            if ((c >= 50) && (c < 100)) {
-                bil1 = c / 50;
-                System.out.println("uang Rp     50 sebanyak " + bil1 + "lembar");
-            }
-
-            // kurang 50
-            c = c % 50;
-            if ((c > 0) && (c < 50)) {
-                bil1 = c;
-                // satuan
-                if (bil1 < 10)
-                    System.out.println("Rp      " + bil1 + " di donasikan");
-                else if (bil1 >= 10)// puluhan
-                    System.out.println("Rp     " + bil1 + " di donasikan");
-            }
+            while (c != 0)
+                ;
+        } catch (Exception e) {
+            System.out.println("maaf pastikan inputan anda adalah angka");
         }
-        while (c != 0)
-            ;
-
     }
 }
