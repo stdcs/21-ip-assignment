c6a3d84113706ffad9ca9bf0f18d8f5fea3be966
erwinarif31 <muherwinarif31@gmail.com>
2021-10-26 01:12:56 +0800

    feat(loop/Eyes): add `nested if` statement to print identity matrix

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -16,6 +16,7 @@ public class Eyes {
         Scanner scan = new Scanner(System.in);
 
         int size;
+
         try {
             size = scan.nextInt();
 
@@ -25,6 +26,18 @@ public class Eyes {
         } catch (Exception e) {
             System.out.println("Masukkan input bilangan bulat >= 0");
             return;
+        }
+
+        for (int i = 0; i < size ; i++) {
+            for (int j = 0; j < size ; j++) {
+                if (j == i) {
+                    System.out.printf("1 ");
+                } else {
+                    System.out.printf("0 ");
+                }
+            }
+
+            System.out.printf("\n");
         }
     }
 }
