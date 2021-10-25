36921153cefd15df202c22fdb79bfb0951c3ee81
Hafieddz <hasmy41@gmail.com>
2021-10-25 22:47:55 +0800

    feat(loop):add new java file

---
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -0,0 +1,16 @@
+package ip.syssrc.loop;
+
+/**
+ * Eyes
+ *
+ * Assignment 2.3
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrhm17h@student.unhas.ac.id>
+ *
+ */
+public class Eyes {
+
+    public static void main(String[] args) {
+
+    }
+}
