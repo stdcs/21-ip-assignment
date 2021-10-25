25b7346c9dc924026dda3f0fed8705a5cb3aae39
FjrSdq <fsiddiq072@gmail.com>
2021-10-25 23:12:24 +0800

    Update Cashier.java

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -12,18 +12,24 @@ public class Cashier {
     public static void main(String[] args) {
         Scanner In = new Scanner(System.in);
         System.out.println("Berapa total harga belanja anda ? ");
-        
-        int totalBelanja = In.nextInt();
-        try{
-        System.out.println("Berapa total uang anda bayarkan ? ");
-            int pembayaran = In.nextInt();
-
-            if (totalBelanja > pembayaran) {
-                System.out.println("Uang Anda Kurang");
+        int totalBelanja = 0;
+        int kembalian = 0;
+        int pembayaran = 0;
+        try {
+            totalBelanja = In.nextInt();
+            System.out.println("Berapa total uang anda bayarkan ? ");
+            pembayaran = In.nextInt();  
+        } catch (Exception e) {
+            System.out.println("Maaf, pastikan inputan anda adalah angka");
+            //TODO: handle exception
+        } 
+        if (totalBelanja > pembayaran) {
+            System.out.println("Uang Anda Kurang");
             } else {
                 int kembalian = pembayaran - totalBelanja;
                 System.out.println("Total kembalian anda = " + kembalian);
                 System.out.println("Dengan dengan rincian sebagai berikut :");
+<<<<<<< HEAD
                 int a,b,c,d,e,f,g,h,i,j;
                 a = 0;
                 b = 0;
@@ -124,14 +130,70 @@ public class Cashier {
                 }
                 if (kembalian > 0) {
                     System.out.println("Dan sebanyak Rp. " + kembalian + " didonasikan");
+=======
+                if (kembalian >= 100000) {
+                    int pecahan = (kembalian / 100000);
+                    kembalian -= (100000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 100.000");
                 }
+                if (kembalian >= 50000) {
+                    int pecahan = (kembalian / 50000);
+                    kembalian -= (50000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 50.000");
+                }
+                if (kembalian >= 20000) {
+                    int pecahan = (kembalian / 20000);                    
+                    kembalian -= (20000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 20.000");
+                }
+                if (kembalian >= 10000) {
+                    int pecahan = (kembalian / 10000);                   
+                    kembalian -= (10000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 10.000");
+                }
+                if (kembalian >= 5000) {
+                    int pecahan = (kembalian / 5000);                    
+                    kembalian -= (5000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 5000");
+                }   
+                if (kembalian >= 2000) {
+                    int pecahan = (kembalian / 2000);                   
+                    kembalian -= (2000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 2000");
+                }
+                if (kembalian >= 1000) {
+                    int pecahan = (kembalian / 1000);                   
+                    kembalian -= (1000 * pecahan);
+                    System.out.println(pecahan + " lembar uang Rp. 1000");
+                }
+                if (kembalian >= 500) {
+                    int pecahan = (kembalian / 500);
+                    kembalian -= (500 * pecahan);
+                    System.out.println(pecahan + " buah uang Rp. 500");
+                }
+                if (kembalian >= 200) {
+                    int pecahan = (kembalian / 200);
+                    kembalian -= (200 * pecahan); 
+                    System.out.println(pecahan + " buah uang Rp. 200");
+                }
+                if (kembalian >= 100) {
+                    int pecahan = (kembalian / 100);
+                    kembalian -= (100 * pecahan);
+                    System.out.println(pecahan + " buah uang Rp. 100");
+>>>>>>> 8830c68 (Update Cashier.java)
+                }
+                In.close();
         
             }
+<<<<<<< HEAD
         } catch (Exception e) {
             System.out.println("Maaf, pastikan input anda adalah angka");
             //TODO: handle exception
         }
        
         In.close();
+=======
+
+>>>>>>> 8830c68 (Update Cashier.java)
     }
 }
