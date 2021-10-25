1904e75d7b8727c396dc049b3922a71b7d7c0445
erwinarif31 <muherwinarif31@gmail.com>
2021-10-26 00:54:28 +0800

    feat(loop/FactorialSequence): add `for` loop to calculate factorial
    And change `n` to `number`

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -15,19 +15,33 @@ public class FactorialSequence {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int n, sum;
+        int number, sum;
         sum = 1;
 
         try {
-            n = scan.nextInt();
+            number = scan.nextInt();
             
-            if (n < 0) {
+            if (number < 0) {
                 throw new Exception();
             }
         } catch (Exception e) {
             System.out.print("Pastikan input adalah bilangan bulat >= 0");
+            return;
         }
 
-        
+        System.out.printf("%d!", number);
+        if (number != 0) {
+            for (int i = number; i >= 1; i--) {
+                sum*=i;
+                if (i == number) {
+                    System.out.printf(" = %d", i);
+                } else {
+                    System.out.printf(" x %d", i);
+                }
+            }
+        } else {
+            sum = 1;
+        }
+        System.out.printf(" = %d", sum);
     }
 }
