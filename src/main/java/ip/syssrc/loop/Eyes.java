2772d0d6eb7d9ced91b069065ecb1107b5205dcc
erwinarif31 <muherwinarif31@gmail.com>
2021-10-25 22:30:23 +0800

    feat(loop): add assignment 2.3, `Eyes.java`

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -1,7 +1,5 @@
 package ip.syssrc.loop;
 
-import java.util.Scanner;
-
 /**
  * Eyes
  *
@@ -13,36 +11,5 @@ import java.util.Scanner;
 public class Eyes {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        int size;
-
-        try {
-            size = scan.nextInt();
-            scan.close();
-
-            if (size <= 0) {
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            System.out.println("Masukkan input bilangan bulat >= 0");
-            return;
-        }
-
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (j == i) {
-                    System.out.printf("%d", 1);
-                } else {
-                    System.out.printf("%d", 0);
-                }
-
-                if (j < (size - 1)) {
-                    System.out.print(" ");
-                }
-            }
-
-            System.out.printf("\n");
-        }
     }
 }
