1411adb8c56f47b36d4f37edef0b998cdc7f1de7
Hafieddz <hasmy41@gmail.com>
2021-10-25 22:47:55 +0800

    feat(loop):add new java file

---
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -0,0 +1,17 @@
+package ip.syssrc.loop;
+
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *
+ */
+
+public class MatricesPad {
+
+    public static void main(String[] args) {
+
+    }
+}
