603035bfd5e9a1eec4d8da49fcf162b16d798dfa
erwinarif31 <muherwinarif31@gmail.com>
2021-10-26 00:17:28 +0800

    feat(loop/MatricesPad): add `for` loop to print output

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -16,5 +16,44 @@ public class MatricesPad {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+<<<<<<< HEAD
+=======
+        int start, end, size;
+
+        try {
+            start = scan.nextInt();
+            end = scan.nextInt();
+            size = scan.nextInt();
+
+            if (start < 0 || end < 0 || size < 0) {
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            System.out.println("Masukkan bilangan bulat >= 0");
+            return;
+        }
+
+        int biggest = (start > end) ? start : end;
+        int leadingZero = 0;
+        while (biggest > 0) {
+            biggest /= 10;
+            leadingZero++;
+        }
+
+        for (int i = start; (start < end ? i <= end : i >= end); ) {
+            if (((i - start) % size == 0 || (start - i) % size == 0) 
+                    && (i != start)) {
+                System.out.printf("\n");
+            }
+
+            System.out.printf("%0" + leadingZero+ "d ", i);
+            if (start < end) {
+                i++;
+            } else {
+                i--;
+            }
+        }
+
+>>>>>>> 018b1c6 (feat(loop/MatricesPad): add `for` loop to print output)
     }
 }
