96b921b20d5c38c34b6e300be1f32318d275d618
erwinarif31 <muherwinarif31@gmail.com>
2021-10-25 22:22:56 +0800

    feat(loop): add assignment 2.2, `FactorialSequence.java`

---
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -0,0 +1,16 @@
+package ip.syssrc.loop;
+
+/**
+ * FactorialSequence
+ *
+ * Assignment 2.2
+ *
+ * @author H071211059 - Muhammad Erwin Arif <muherwinarif31@gmail.com>
+ *
+ */
+public class FactorialSequence {
+
+    public static void main(String[] args) {
+
+    }
+}
