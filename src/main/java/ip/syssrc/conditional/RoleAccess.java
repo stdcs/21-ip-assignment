cf782d8eb1281a22c8b68447bf546569e6e2b28d
FjrSdq <fsiddiq072@gmail.com>
2021-10-25 16:54:04 +0800

    Update RoleAccess.java

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -50,9 +50,9 @@ public class RoleAccess {
         } else if (args.length == 1) {
 
             switch (args[0]) {
-                case "Super Admin";
-                case "Admin";
-                case "User";
+                case "Super Admin":
+                case "Admin":
+                case "User":
                     isValidRole = true;
                     break;
                 default:
