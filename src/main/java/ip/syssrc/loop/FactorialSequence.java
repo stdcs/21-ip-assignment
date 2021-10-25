2c4ad1609f360a67c4f46d24ccc34699d86ca320
erwinarif31 <muherwinarif31@gmail.com>
2021-10-26 00:41:05 +0800

    feat(loop/FactorialSequence): declare `n`, `sum` variable

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -1,6 +1,7 @@
 package ip.syssrc.loop;
 
 import java.util.Scanner;
+
 /**
  * FactorialSequence
  *
@@ -12,11 +13,7 @@ import java.util.Scanner;
 public class FactorialSequence {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
         Scanner scan = new Scanner(System.in);
 
-        
->>>>>>> 66bd1f0 (feat(loop/FactorialSequence): add `java.util.Scanner` object)
     }
 }
--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -16,16 +16,13 @@ public class MatricesPad {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-<<<<<<< HEAD
-=======
         int start, end, size;
 
         try {
             start = scan.nextInt();
             end = scan.nextInt();
             size = scan.nextInt();
-            scan.close();
-            
+
             if (start < 0 || end < 0 || size < 0) {
                 throw new Exception();
             }
@@ -41,13 +38,12 @@ public class MatricesPad {
             leadingZero++;
         }
 
-        for (int i = start; (start < end ? i <= end : i >= end); ) {
-            if (((i - start) % size == 0 || (start - i) % size == 0) 
-                    && (i != start)) {
+        for (int i = start; (start < end ? i <= end : i >= end);) {
+            if (((i - start) % size == 0 || (start - i) % size == 0) && (i != start)) {
                 System.out.printf("\n");
             }
 
-            System.out.printf("%0" + leadingZero+ "d ", i);
+            System.out.printf("%0" + leadingZero + "d ", i);
             if (start < end) {
                 i++;
             } else {
@@ -55,6 +51,5 @@ public class MatricesPad {
             }
         }
 
->>>>>>> 018b1c6 (feat(loop/MatricesPad): add `for` loop to print output)
     }
 }
