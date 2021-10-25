c60b22267b78e7685b710898352578ae707ab5d3
sofyanox12 <gaero38@gmail.com>
2021-10-26 06:24:56 +0900

    Update : Change money Mechanism
    bug : Kembalian yang nyata untuk pecahan kecil harga dengan donasi yang terlalu besar.
    fix : Menambahkan objek pecahan untuk setiap kondisi pengembalian.

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -29,6 +29,7 @@ public class Cashier {
 
         int totalBelanja = 0;
         int pembayaran = 0;
+        int pecahan = 0;
 
         // Interface sederhana
         System.out.println("\n-----[WELCOME TO SYSMART CASHIER]-----");
@@ -60,43 +61,53 @@ public class Cashier {
 
             // Seleksi Kondisi
             if (kembalian >= 100000) {
-                kembalian = kembalian - 100000;
+                pecahan = (kembalian / 100000);
+                kembalian = kembalian - (100000 * pecahan);
                 seratusRibu = seratusRibu + 1;
             }
             if (kembalian >= 50000) {
-                kembalian = kembalian - 50000;
+                pecahan = (kembalian / 50000);
+                kembalian = kembalian - (50000 * pecahan);
                 limaPuluhRibu = limaPuluhRibu + 1;
             }
             if (kembalian >= 20000) {
-                kembalian = kembalian - 20000;
+                pecahan = (kembalian / 20000);
+                kembalian = kembalian - (20000 * pecahan);
                 duaPuluhRibu = duaPuluhRibu + 1;
             }
             if (kembalian >= 10000) {
-                kembalian = kembalian - 10000;
+                pecahan = (kembalian / 10000);
+                kembalian = kembalian - (10000 * pecahan);
                 sepuluhRibu = sepuluhRibu + 1;
             }
             if (kembalian >= 5000) {
-                kembalian = kembalian - 5000;
+                pecahan = (kembalian / 5000);
+                kembalian = kembalian - (5000 * pecahan);
                 limaRibu = limaRibu + 1;
             }   
             if (kembalian >= 2000) {
-                kembalian = kembalian - 2000;
+                pecahan = (kembalian / 2000);
+                kembalian = kembalian - (2000 * pecahan);
                 duaRibu = duaRibu + 1;
             }
             if (kembalian >= 1000) {
-                kembalian = kembalian - 1000;
+                pecahan = (kembalian / 1000);
+                kembalian = kembalian - (1000 * pecahan);
                 seribu = seribu + 1;
             }
             if (kembalian >= 500) {
-                kembalian = kembalian - 500;
+                pecahan = (kembalian / 500);
+                kembalian = kembalian - (500 * pecahan);
                 limaRatus = limaRatus + 1;
             }
             if (kembalian >= 200) {
-                kembalian = kembalian - 200;
+                pecahan = (kembalian / 200);
+                kembalian = kembalian - (200 * pecahan);
                 duaRatus = duaRatus + 1;
             }
             if (kembalian >= 100) {
-                kembalian = kembalian - 100;
+                pecahan = (kembalian / 100);
+                kembalian = kembalian - (100 * pecahan);
                 seratus = seratus + 1;
             }
             if (seratusRibu > 0) {
@@ -121,13 +132,13 @@ public class Cashier {
                 System.out.println(seribu + " lembar uang Rp.1000");
             } 
             if (limaRatus > 0) {
-                System.out.println(limaRatus + " lembar uang Rp.500");
+                System.out.println(limaRatus + " buah uang Rp.500");
             } 
             if (duaRatus > 0) {
-                System.out.println(duaRatus + " lembar uang Rp.200");
+                System.out.println(duaRatus + " buah uang Rp.200");
             } 
             if (seratus > 0) {
-                System.out.println(seratus + " lembar uang Rp.100");
+                System.out.println(seratus + " buah uang Rp.100");
             }
             if (kembalian > 0) {
                 System.out.println("Dan sebanyak Rp. " + kembalian + " didonasikan");
