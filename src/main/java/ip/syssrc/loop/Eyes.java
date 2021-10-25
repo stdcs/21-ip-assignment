66651cf38ae8bdac00c43a96261e00f299e7d097
erwinarif31 <muherwinarif31@gmail.com>
2021-10-25 22:30:23 +0800

    feat(loop): add assignment 2.3, `Eyes.java`

---
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -0,0 +1,16 @@
+package ip.syssrc.loop;
+
+/**
+ * Eyes
+ *
+ * Assignment 2.3
+ *
+ * @author H071211059 - Muhammad Erwin Arif <muherwinarif31@gmail.com>
+ *
+ */
+public class Eyes {
+
+    public static void main(String[] args) {
+
+    }
+}
