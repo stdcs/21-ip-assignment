0f22177215b2c1459609e3684360ebc5cf33f13e
Hafieddz <hasmy41@gmail.com>
2021-10-25 22:58:46 +0800

    feat(loop/MatricesPad.java): add 'start' 'end' 'size' as int variabel

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -14,6 +14,9 @@ public class MatricesPad {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int start = in.nextInt();
+        int end = in.nextInt();
+        int size = in.nextInt();
         
         int start, size, end;
 
