00ded2c9460a53a66ad7f2d116d1747f0512e472
FjrSdq <fsiddiq072@gmail.com>
2021-10-26 22:52:48 +0800

    Update FactorialSequence.java

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
  *
  * Assignment 2.2
  *
+<<<<<<< HEAD
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+ * @author H0711211078 Muh Fajar Siddiq <fsiddiq072@gmail.com>
+=======
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+>>>>>>> 509d200 (feat(loop) : add assigment 2.1 2.2 2.3)
+=======
+ * @author H0711211078 Muh Fajar Siddiq <fsiddiq072@gmail.com>
+>>>>>>> aca5ce6 (Update FactorialSequence.java)
+>>>>>>> f2ae1a1 (Update FactorialSequence.java)
  *
  */
 public class FactorialSequence {
