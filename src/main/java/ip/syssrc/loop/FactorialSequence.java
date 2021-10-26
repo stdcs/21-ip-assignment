f95e215c2c7bf5f94df09209cefc92dab532c244
sofyanox12 <gaero38@gmail.com>
2021-10-26 09:39:08 +0900

    add

---
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -0,0 +1,16 @@
+package ip.syssrc.loop;
+
+/**
+ * FactorialSequence
+ *
+ * Assignment 2.2
+ *
+ * @author H071211045 - MUHAMMAD SOFYAN DAUD PUJAS <gaero38@gmail.com>
+ *
+ */
+public class FactorialSequence {
+
+    public static void main(String[] args) {
+
+    }
+}
