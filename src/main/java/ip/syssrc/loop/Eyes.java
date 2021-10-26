c9b229e4a11eb1e650db41ea3338907a7afc6093
FjrSdq <fsiddiq072@gmail.com>
2021-10-26 22:32:26 +0800

    feat(loop) : add assigment 2.1 2.2 2.3
    `MatricesPad.java` `FactorialSequence.java` `Eyes.java`

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  *
  * Assignment 2.3
  *
- * @author H071211078 - Muh Fajar Siddiq <fsiddiq072@gmail.com>
+ * @author H071171512 - Fitrah Muhammad <fitrhm17h@student.unhas.ac.id>
  *
  */
 public class Eyes {
@@ -13,34 +13,17 @@ public class Eyes {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int n;
+        int n = in.nextInt();
 
-        try {
-            n = in.nextInt();
-            in.close();
-
-            if (n <= 0){
-                throw new Exception();
-            }
-        } catch (Exception e) {
-            System.out.println("Input Error");
-            return;
-            //TODO: handle exception
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j == i) {
-                    System.out.printf("%d", 1); 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print(1 + " ");
                 } else {
-                    System.out.printf("%d", 0);
-                }
-                
-                if ( j < (n - 1)){
-                    System.out.print(" ");
+                    System.out.print(0 + " ");
                 }
             }
-            System.out.printf("\n");
+            System.out.println();
         }
     }
 }
