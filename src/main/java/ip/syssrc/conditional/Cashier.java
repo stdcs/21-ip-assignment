b6afd0018428ad5347357601ee4d5ada155c2513
Djulizah <djul.b74123@gmail.com>
2021-10-26 21:00:27 +0800

    fix(conditional/Cashier): closing curly braces

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -51,56 +51,75 @@ public class Cashier {
             if (kembalian >= 100000) {
                 kembalian = kembalian - 100000;
                 jumlah100K = jumlah100K + 1;
-            } if (kembalian >= 50000) {
+            }
+            if (kembalian >= 50000) {
                 kembalian = kembalian - 50000;
                 jumlah50K = jumlah50K + 1;
-            } if (kembalian >= 20000) {
+            }
+            if (kembalian >= 20000) {
                 kembalian = kembalian - 20000;
                 jumlah20K = jumlah20K + 1;
-            } if (kembalian >= 10000) {
+            }
+            if (kembalian >= 10000) {
                 kembalian = kembalian - 10000;
                 jumlah10K = jumlah10K + 1;
-            } if (kembalian >= 5000) {
+            }
+            if (kembalian >= 5000) {
                 kembalian = kembalian - 5000;
                 jumlah5K = jumlah5K + 1;
-            } if (kembalian >= 2000) {
+            }
+            if (kembalian >= 2000) {
                 kembalian = kembalian - 2000;
                 jumlah2K= jumlah2K + 1;
-            } if (kembalian >= 1000) {
+            }
+            if (kembalian >= 1000) {
                 kembalian = kembalian - 1000;
                 jumlah1K = jumlah1K + 1;
-            } if (kembalian >= 500) {
+            }
+            if (kembalian >= 500) {
                 kembalian = kembalian - 500;
                 jumlah500 = jumlah500 + 1;
-            } if (kembalian >= 200) {
+            }
+            if (kembalian >= 200) {
                 kembalian = kembalian - 200;
                 jumlah200 = jumlah200 + 1;
-            } if (kembalian >= 100) {
+            }
+            if (kembalian >= 100) {
                 kembalian = kembalian - 100;
                 jumlah100 = jumlah100 + 1;
             }
             
             if (jumlah100K > 0) {
                 System.out.println(a + " lembar uang Rp.100000");
-            } if (jumlah50K > 0) {
+            }
+            if (jumlah50K > 0) {
                 System.out.println(b + " lembar uang Rp.50000");
-            } if (jumlah20K > 0) {
+            }
+            if (jumlah20K > 0) {
                 System.out.println(c + " lembar uang Rp.20000");
-            } if (jumlah10K > 0) {
+            }
+            if (jumlah10K > 0) {
                 System.out.println(d + " lembar uang Rp.10000");
-            } if (jumlah5K > 0) {
+            }
+            if (jumlah5K > 0) {
                 System.out.println(e + " lembar uang Rp.5000");
-            } if (jumlah2K > 0) {
+            }
+            if (jumlah2K > 0) {
                 System.out.println(f + " lembar uang Rp.2000");
-            } if (jumlah1K > 0) {
+            }
+            if (jumlah1K > 0) {
                 System.out.println(g + " lembar uang Rp.1000");
-            } if (jumlah500 > 0) {
+            }
+            if (jumlah500 > 0) {
                 System.out.println(h + " buah uang Rp.500");
-            } if (jumlah200 > 0) {
+            }
+            if (jumlah200 > 0) {
                 System.out.println(i + " buah uang Rp.200");
-            } if (jumlah100 > 0) {
+            }
+            if (jumlah100 > 0) {
                 System.out.println(j + " buah uang Rp.100");
-            } if (kembalian > 0) {
+            }
+            if (kembalian > 0) {
                 System.out.println("Dan sebanyak Rp. " + kembalian + " didonasikan");
             }
 
