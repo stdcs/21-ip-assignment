7e043bec04b18737543fca7043fabad6f6fda7b3
sofyanox12 <gaero38@gmail.com>
2021-10-26 09:39:08 +0900

    add

---
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -0,0 +1,17 @@
+package ip.syssrc.loop;
+
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071211045 - MUHAMMAD SOFYAN DAUD PUJAS <gaero38@gmail.com>
+ *
+ */
+
+public class MatricesPad {
+
+    public static void main(String[] args) {
+
+    }
+}
