d8ab39a0a93737877357ba0800e1013233411369
Liska <rombeliska@gmail.com>
2021-10-28 00:22:53 +0800

    feat (loop/Eyes):add code2

---
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -0,0 +1,30 @@
+package ip.syssrc.loop;
+
+/**
+ * Eyes
+ *
+ * Assignment 2.3
+ *
+ * @author H071211083 - Liska Dewi Rombe <rombeliska@gmail.com>
+ *
+ */
+public class Eyes {
+
+    public static void main(String[] args) {
+        Scanner li = new Scanner(System.in);
+
+        int n = li.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
