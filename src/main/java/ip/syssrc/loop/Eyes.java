66eeb9312940005417d7ef5bb6da78ff07d2b582
stalyaardani <stalyaardaniaries@gmail.com>
2021-10-27 21:36:23 +0800

    feat (loop/Eyes) : add import java.util.Scanner and variable integer

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -1,16 +1,20 @@
 package ip.syssrc.loop;
-
+import java.util.Scanner;
 /**
  * Eyes
  *
  * Assignment 2.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrhm17h@student.unhas.ac.id>
+ * @author H071211061 - St. Alya Ardani Aries <stalyaardaniaries@gmail.com>
  *
  */
 public class Eyes {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+
+        
 
     }
 }
