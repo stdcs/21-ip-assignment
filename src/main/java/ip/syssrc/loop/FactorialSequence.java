976e0d80c3da3543b73ebf2d33633d7627cff26d
Hafieddz <hasmy41@gmail.com>
2021-10-27 17:00:43 +0800

    feat(loop\FactorialSequence.java): add 'java.util.Scanner' object

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -1,6 +1,7 @@
 package ip.syssrc.loop;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 import java.util.Scanner;
 
@@ -10,11 +11,19 @@ import java.util.Scanner;
 import java.util.Scanner;
 >>>>>>> 837fe87 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
 =======
+=======
+>>>>>>> 6380848 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
 import java.util.Scanner;
 =======
 
 >>>>>>> 1411adb (feat(loop):add new java file)
+<<<<<<< HEAD
 >>>>>>> c536015 (feat(loop):add new java file)
+=======
+=======
+import java.util.Scanner;
+>>>>>>> 837fe87 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
+>>>>>>> 6380848 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
 /**
  * FactorialSequence
  *
@@ -22,6 +31,7 @@ import java.util.Scanner;
  *
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  *
  * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
  *
@@ -30,16 +40,25 @@ import java.util.Scanner;
  * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
 >>>>>>> 837fe87 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
 =======
+=======
+>>>>>>> 6380848 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
  * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
 =======
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
 >>>>>>> 1411adb (feat(loop):add new java file)
+<<<<<<< HEAD
 >>>>>>> c536015 (feat(loop):add new java file)
+=======
+=======
+ * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
+>>>>>>> 837fe87 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
+>>>>>>> 6380848 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
  *
  */
 public class FactorialSequence {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
 <<<<<<< HEAD
         Scanner in = new Scanner(System.in);
 <<<<<<< HEAD
@@ -98,6 +117,13 @@ public class FactorialSequence {
 =======
 
 >>>>>>> 1411adb (feat(loop):add new java file)
+<<<<<<< HEAD
 >>>>>>> c536015 (feat(loop):add new java file)
+=======
+=======
+        Scanner in = new Scanner(System.in);
+        
+>>>>>>> 837fe87 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
+>>>>>>> 6380848 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
     }
 }
