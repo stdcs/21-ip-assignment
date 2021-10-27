9838563439d318fb6205e5674165c4c05ee3ec87
Hafieddz <hasmy41@gmail.com>
2021-10-27 21:31:36 +0800

    feat(loop/FactorialSequence.java): add if else statement with loop

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -55,8 +55,29 @@ public class FactorialSequence {
 
         long factorial = 1;
         int n = in.nextInt();
+<<<<<<< HEAD
 >>>>>>> 69e510c (feat(loop/FactorialSequence.java): add variabel)
         
 >>>>>>> 837fe87 (feat(loop\FactorialSequence.java): add 'java.util.Scanner' object)
+=======
+
+        if (n > 0){
+            System.out.print(n + "! = ");
+
+            for (int i = n ; i >= 2; i--){
+                System.out.print(i + " x ");
+                factorial = factorial * i;
+            }
+            System.out.print("1 = " + factorial);
+        
+        } else if (n == 0){
+            System.out.print(n + "! = ");
+
+            System.out.println("1");
+        } else {
+            System.out.println("Error!");
+        }
+        in.close();
+>>>>>>> 7017a14 (feat(loop/FactorialSequence.java): add if else statement with loop)
     }
 }
