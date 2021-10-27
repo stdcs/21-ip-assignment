42028059a4854225b4a71b8e74a77cb533f2a413
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-10-27 14:54:48 +0700

    feature(loop/Eyes): Initialize Variable And Scanner object

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -13,7 +13,22 @@ public class Eyes {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        
+        int n;
+        System.out.print("Input N = ");
+        n = sc.nextInt();
+      
+        for(int i = 0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if( i == j) {
+                    System.out.print("1 ");
+                }
+                else { 
+                    System.out.print("0 ");
+                }
+            }
+            System.out.print("\n");
+            sc.close();
+        }
        
     }
 }
