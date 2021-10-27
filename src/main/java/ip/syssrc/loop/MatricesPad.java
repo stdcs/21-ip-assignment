3e0c956c907b9a6018e058fe139be7aad467b04e
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-10-27 17:51:48 +0700

    feature(loop/MatricesPad): Initialize variable, Add Scanner obj

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -5,7 +5,7 @@ package ip.syssrc.loop;
  *
  * Assignment 2.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211064 - Agung Kartika Ardhiyanda <agungardhiyanda25@gmail.com>
  *
  */
 import java.util.Scanner;
@@ -13,19 +13,37 @@ public class MatricesPad {
 
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(Systemin);
+        Scanner sc = new Scanner(System.in);
 
-        int start;
-        int end;
-        int i;
-        int size;
+        int start,end,i,size;
         System.out.print("Start (Bilangan Bulat >= 0 ) = ");
         start = sc.nextInt();
         System.out.print("End (Bilangan Bulat >= 0 ) = ");
         end = sc.nextInt();
         System.out.print("Size (Bilangan Bulat >= 0 ) = ");
         size = sc.nextInt();
+
+        int terbesar = (start > end) ? start : end;
+        int leadingZero = 0;
         
-        i = start;
+        while (terbesar > 0 ){
+            terbesar /=10;
+            leadingZero++;
+        }
+
+        for (i = start; (start < end ? i <= end : i >= end);) {
+            if (((start - i ) % size == 0 || (i - start) % size == 0)
+            && (i != start)) {
+                System.out.printf("\n");
+            }
+
+            System.out.printf("%0" + leadingZero+ "d ", i);
+            if(start < end){
+                i++;
+            } else {
+                i--;
+            }
+            sc.close();
+        }
     }
 }
