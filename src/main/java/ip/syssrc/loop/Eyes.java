dadac8c429eb4191866863a6be2a52178d07745e
Hafieddz <hasmy41@gmail.com>
2021-10-27 21:37:45 +0800

    feat(loop/Eyes.java): add 'java.util.Scanner' object

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -1,16 +1,17 @@
 package ip.syssrc.loop;
-
+import java.util.Scanner;
 /**
  * Eyes
  *
  * Assignment 2.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrhm17h@student.unhas.ac.id>
+ * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
  *
  */
 public class Eyes {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        
     }
 }
