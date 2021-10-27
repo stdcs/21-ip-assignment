ef38dbcc6173b0b0cef153a32f91ba65851caa37
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-10-27 14:24:33 +0700

    feature(looping/Eyes): Add Scanner object

---
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -0,0 +1,20 @@
+package ip.syssrc.loop;
+
+/**
+ * Eyes
+ *
+ * Assignment 2.3
+ *
+ * @author H071211064 - Agung Kartika Ardhiyanda <agungardhiyanda25@gmail.com>
+ *
+ */
+import java.util.Scanner;
+public class Eyes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+    }
+}
