9a1706a146dc6fd947aa14661b67db135c84b67b
SyifaUrRahmi <sifasisi3@gmail.com>
2021-10-27 13:27:24 +0800

    feat(conditional/FactorialSequence) : Membuat programnya

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -17,6 +17,20 @@ public class FactorialSequence {
         int n = in.nextInt();
         long factorial = 1;
 
-        
+        if (n > 0) {
+            System.out.print(n + "! = ");
+
+            for (int i = n; i >= 2; i--) {
+                System.out.print(i + " x ");
+                factorial = factorial * i;
+            }
+
+            System.out.print("1 = " + factorial);
+
+        } else if (n == 0) {
+            System.out.print(n + "! = ");
+
+            System.out.println("1");
+        }
     }
 }
