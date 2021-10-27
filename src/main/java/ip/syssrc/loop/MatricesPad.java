9bed9be4eb6de17775586945bbdb4c01ed41a441
Hafieddz <hasmy41@gmail.com>
2021-10-27 16:21:35 +0800

    feat(loop\MatricesPad.java):add if else statement

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -16,6 +16,38 @@ public class MatricesPad {
         int start = in.nextInt();
         int end = in.nextInt();
         int size = in.nextInt();
+
+        int range = end - start;
+        int temp = start;
         
+        if (start < 10 && end < 10 ){
+            if (range >= 0 ){
+                for (int i = 1; i <= range / size; i++){
+                    for (int j = 1; j <= size; j++){
+                        System.out.printf("%01d ", temp);
+                        temp++;
+                    }
+                    System.out.println(" ");
+                }
+                for (int k = 0; k <= range % size; k++){
+                    System.out.printf("%01d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0){
+                for (int i = 1; i <= -range / size; i++){
+                    for (int j = 1; j <= size; j++){
+                        System.out.printf("%01d ", temp);
+                        temp--;
+                    }
+                    System.out.println(" ");
+                }
+                for (int k = 0; k <= -range % size; k++){
+                    System.out.printf("%01d ", temp);
+                    temp--;
+                }
+            }   
+        }
+        in.close();
     }
 }
+
