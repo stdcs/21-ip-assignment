837fe87d16cd9b9918fc4887624fbef40e19065f
Hafieddz <hasmy41@gmail.com>
2021-10-27 17:00:43 +0800

    feat(loop\FactorialSequence.java): add 'java.util.Scanner' object

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -1,16 +1,17 @@
 package ip.syssrc.loop;
-
+import java.util.Scanner;
 /**
  * FactorialSequence
  *
  * Assignment 2.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
  *
  */
 public class FactorialSequence {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        
     }
 }
