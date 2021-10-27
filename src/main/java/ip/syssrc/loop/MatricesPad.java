8ce2ff0b29c32dc323e4201560335e1d08b41496
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-10-27 20:44:59 +0700

    feat(loop/MatricesPad) : import java.util.Scanner;

---
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -0,0 +1,22 @@
+package ip.syssrc.loop;
+import java.util.Scanner; 
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071211079 - Dhiyaa Unnisa <unnisadhiyaa25@gmail.com>
+ *
+ */
+
+public class MatricesPad {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int start = in.nextInt();
+        int end = in.nextInt();
+        int size = in.nextInt();
+    }
+}
