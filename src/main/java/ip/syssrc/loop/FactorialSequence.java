29682d67994db220c2d4aed52eb789632e6bab79
SyifaUrRahmi <sifasisi3@gmail.com>
2021-10-27 12:22:21 +0800

    feat(conditional/MatricesPad) : Menambahkan variabel start, end, dan size

---
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -0,0 +1,16 @@
+package ip.syssrc.loop;
+
+/**
+ * FactorialSequence
+ *
+ * Assignment 2.2
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *
+ */
+public class FactorialSequence {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -0,0 +1,21 @@
+package ip.syssrc.loop;
+
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071211066 - Syifa Ur Rahmi <sifasisi3@gmail.com>
+ *
+ */
+import java.util.Scanner;
+public class MatricesPad {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int start = in.nextInt();
+        int end = in.nextInt();
+        int size = in.nextInt();
+    }
+}
