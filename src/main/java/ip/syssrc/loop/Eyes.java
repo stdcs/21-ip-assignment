192d72a82e6d031f0142edb3e90a6f972916f2fa
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-10-27 22:24:12 +0800

    feat(loop/Eyes): add format 'for'

---
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -0,0 +1,31 @@
+package ip.syssrc.loop;
+
+/**
+ * Eyes
+ *
+ * Assignment 2.3
+ *
+ * @author H071211074 - Wd. Ananda Lesmono <nandalesmono@gmail.com>
+ *
+ */
+import java.util.Scanner;
+public class Eyes {
+
+    public static void main(String[] args) {
+        Scanner dede = new Scanner (System.in);
+
+        int n = dede.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
