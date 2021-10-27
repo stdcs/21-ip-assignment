b2bc0b4b502654939606205d53d526c5cf22c62c
FjrSdq <fsiddiq072@gmail.com>
2021-10-28 00:13:43 +0800

    feat(loop): input no longer be limited to 3 digits

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -13,94 +13,46 @@ public class MatricesPad {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int start = 0;
+        int end = 0;
+        int size = 0;
+        try {
+            start = in.nextInt();
+            end = in.nextInt();
+            size = in.nextInt();
+            in.close();
 
-        int start = in.nextInt();
-        int end = in.nextInt();
-        int size = in.nextInt();
-
-        int range = end - start;
-        int temp = start;
-
-        if (start <= 9 && end <= 9) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp--;
-                }
+            if(start < 0 || end < 0 || size <0){
+                throw new Exception();
             }
-        } else if (start <= 99 && end <= 99) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 999 && end <= 999) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp--;
-                }
-            }
+        } catch (Exception e) {
+            System.out.println("Tolong masukkan input Bilangan Bulat >= 0");
+            return;
+            //TODO: handle exception
         }
 
-        in.close();
+        /**leading zero */
+        int max = (start > end) ? start : end;
+        int leadZero = 0;
+        while (max > 0) {
+            max /= 10;
+            leadZero++;
+        }
+        /**OUTPUT */
+        for (int i = start; (start < end ? i <= end : i >= end); ){
+            if((i - start) % size == 0 || (start - i) % size == 0 
+            && (i != start)){
+                System.out.printf("\n");
+            }
+
+            System.out.printf("%0" + leadZero + "d ", i);
+            if (start < end ){
+                i++;
+            }else {
+                i--;
+            }
+            
+        }
+       
     }
 }
