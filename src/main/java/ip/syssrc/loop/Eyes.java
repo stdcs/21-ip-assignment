ac4edeaeea768af90cea83d058ebb58ea56e46ef
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-10-27 14:39:55 +0700

    feature(loop/Eyes): Add scanner object, Add for looping

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -12,9 +12,23 @@ import java.util.Scanner;
 public class Eyes {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Scanner sc = new Scanner (System.in);
         
+        int n;
+        System.out.print("Input N = ");
+        n = sc.nextInt();
 
+        for(i = 0; i<n; i++) {
+            for(int j = 0; j < n; j++){
+                if( i == j){
+                    System.out.print("1 ");
+                }
+                else {
+                    System.out.print("0 ");
+                }
+            }
+        }
+        System.out.print("\n");
+        sc.close();
     }
 }
