5275c334938e4ecd3796c4a19c284b0ea897d8f1
Muhammad Faiz Fatwa Syarifuddin <92856595+faizftw@users.noreply.github.com>
2021-10-27 21:07:44 +0800

    feat(loop/FactorialSequence)

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -1,16 +1,39 @@
 package ip.syssrc.loop;
+import java.util.Scanner;
 
 /**
  * FactorialSequence
  *
  * Assignment 2.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211069 - Muhammad Faiz Fatwa Syarifuddin <faizfatwa030206@gmail.com
  *
  */
 public class FactorialSequence {
 
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        long factorial = 1;
+
+        if (n > 0) {
+            System.out.print(n + "! = ");
+
+            for (int i = n; i >= 2; i--) {
+                System.out.print(i + " x ");
+                factorial = factorial * i;
+            }
+            System.out.print("1 = " + factorial);
+        }
+        else if (n == 0) {
+            System.out.print(n + "! = ");
+            System.out.println("1");
+        }
+        else {
+            System.out.println("Error");
+        }
+        in.close();
     }
 }
