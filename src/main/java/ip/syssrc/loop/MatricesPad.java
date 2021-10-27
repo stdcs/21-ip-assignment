d7ccaad57b37381690ea2621fc4fc4546ccbb9cd
Hafieddz <hasmy41@gmail.com>
2021-10-27 16:56:28 +0800

    feat(loop\MatricesPad.java): add more if else statement

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -16,6 +16,7 @@ public class MatricesPad {
         int start = in.nextInt();
         int end = in.nextInt();
         int size = in.nextInt();
+        System.out.println(" ");
 
         int range = end - start;
         int temp = start;
@@ -46,6 +47,56 @@ public class MatricesPad {
                     temp--;
                 }
             }   
+        } else if (start < 100 & end < 100){
+            if (range >= 0){
+                for (int i = 1; i <= range / size; i++){
+                    for (int j= 1; j <= size; j++){
+                        System.out.printf("%02d ", temp);
+                        temp++;
+                    }
+                    System.out.println(" ");
+                }
+                for (int k = 0; k <= range % size; k++){
+                    System.out.printf("%02d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0){
+                for (int i = 1; i <= -range / size; i++){
+                    for (int j = 1; j <= size; j++){
+                        System.out.printf("%02d ", temp);
+                        temp--;
+                    }
+                    System.out.println(" ");
+                }
+                for (int k = 0; k <= - range % size; k++){
+                    System.out.printf("%02d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start < 1000 && end < 1000){
+            if (range >= 0){
+                for (int i = 1; i <= range / size; i++){
+                    for (int j = 1; j <= size; j++){
+                        System.out.printf("%03d ", temp);
+                        temp++;
+                    }
+                    System.out.println(" ");
+                }
+                for (int k = 0; k <= range % size; k++){
+                    System.out.printf("%03d", temp);
+                }
+            } else if (range <= 0){
+                for (int i = 1; i <= -range / size; i++){
+                    for (int j = 1; j <= size; j++){
+                        System.out.printf("03d ", temp);
+                        temp--;
+                    }
+                    System.out.println(" ");
+                }
+                for (int k = 0; k <= -range % size; k++){
+                    System.out.printf("%03d ", temp);
+                }
+            }
         }
         in.close();
     }
