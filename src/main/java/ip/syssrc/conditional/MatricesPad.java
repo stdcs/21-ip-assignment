953bda097a2cfa7ea58f4bb3daf76527e75e8082
dirganoob <92706538+dirganoob@users.noreply.github.com>
2021-10-27 19:30:06 +0800

    Update Eyes.java

---
+++ src/main/java/ip/syssrc/conditional/MatricesPad.java
@@ -0,0 +1,35 @@
+package ip.syssrc.loop;
+
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071121080 - Dirgantry Leonard Nugrah Boro <dirgantryleonardnugrahboro@gmail.com>
+ *
+ */
+
+import java.util.Scanner;
+ public class MatricesPad {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int dummy = 0, n = 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j == dummy) System.out.printf("%d ", n);
+                else System.out.printf("0 ");
+            }
+            dummy++;
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+    }
+}
