74617ecdd40fe6cf4d8975985ef5bc0d0e1c7d1f
Suhastina <Suhastina88@gmail.com>
2021-10-27 23:51:54 +0700

    feat(loop/Eyes): add code

---
+++ src/main/java/ip/syssrc/Loop/FactorialSequence.java
@@ -0,0 +1,41 @@
+package main.java.ip.syssrc.loop;
+// error jika menggunakan package ip.syssrc.loop;
+/**
+ * FactorialSequence
+ * 
+ * Assignment 2.2
+ * 
+ * @author H071211084 - Suhastina <Suhastina88@gmail.com>
+ * 
+ */
+import java.util.Scanner;
+
+public class FactorialSequence {
+    public static void main(String[] args) {
+        Scanner t2 = new Scanner(System.in);
+
+        long fact = 1;
+        int n = t2.nextInt();
+
+        if (n > 0) {
+            System.out.print(n + "! = ");
+
+            for (int i = n; i >= 2; i--) {
+                System.out.print(i + " x ");
+                fact = fact * i;
+            }
+            //error 
+            System.out.print("1 = " + fact);
+
+        } else if (n == 0) {
+            System.out.print(n + "! = ");
+
+            System.out.println("1");
+        } else {
+            System.out.println("Error");
+        }
+
+        t2.close();
+    }
+    
+}
