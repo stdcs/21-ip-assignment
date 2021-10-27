deb9994d4986dd426d9b6bd04921ce8f45c26fd3
SyifaUrRahmi <sifasisi3@gmail.com>
2021-10-27 13:17:41 +0800

    feat(conditional/FactorialSequence) : import java.util.Scanner

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -5,12 +5,13 @@ package ip.syssrc.loop;
  *
  * Assignment 2.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211066 - Syifa Ur Rahmi <sifasisi3@gmail.com>
  *
  */
+import java.util.Scanner;
 public class FactorialSequence {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
     }
 }
