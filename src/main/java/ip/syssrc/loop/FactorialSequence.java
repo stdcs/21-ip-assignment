c1b213baff869f1ef264ef6020df720dc55857b4
Liska <rombeliska@gmail.com>
2021-10-28 00:22:53 +0800

    feat (loop/Eyes):add code2

---
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -0,0 +1,38 @@
+package ip.syssrc.loop;
+
+/**
+ * FactorialSequence
+ *
+ * Assignment 2.2
+ *
+ * @author H071211083 - Liska Dewi Rombe <rombeliska@gmail.com>
+ *
+ */
+public class FactorialSequence {
+
+    public static void main(String[] args) {
+        long fact = 1;
+        int n = lio.nextInt();
+
+        if (n > 0) {
+            System.out.print(n + "! = ");
+
+            for (int i = n; i >= 2; i--) {
+                System.out.print(i + " x ");
+                fact = fact * i;
+            }
+
+            System.out.print("1 = " + fact);
+
+        } else if (n == 0) {
+            System.out.print(n + "! = ");
+
+            System.out.println("1");
+        } else {
+            System.out.println("Error");
+        }
+
+        lio.close();
+    }
+
+    }
