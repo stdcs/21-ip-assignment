d8e6dfd2319178b1e1106b9e444bb3f61a1f4720
Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
2021-10-28 02:58:36 +0800

    feat(loop): add assigment `MatricesPad` `FactorialSequence` `Eyes`

---
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -0,0 +1,30 @@
+package ip.syssrc.loop;
+
+/**
+ * Eyes
+ *
+ * Assignment 2.3
+ *
+ * @author H071211065 - Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
+ *
+ */
+public class Eyes {
+
+    public static void main(String[] args) {
+        Scanner bk = new Scanner(System.in);
+
+        int n = bk.nextInt();
+        bk.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        } 
+    } 
+}
+
