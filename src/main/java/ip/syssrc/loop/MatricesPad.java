d93dbffc3dd015f53339fabd2119ddd180329b60
FIRMANSYAH <frmnsyah33@gmail.com>
2021-10-27 20:53:38 +0800

    feat (loop):add assigment

---
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -0,0 +1,109 @@
+package ip.syssrc.loop;
+
+import java.util.Scanner;
+
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071211070 - Firmansyah <frmnsyah33@gmail.com>
+ *
+ */
+
+public class MatricesPad {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int start = scan.nextInt();
+        int end = scan.nextInt();
+        int size = scan.nextInt();
+
+        int range = end - start;
+        int temp = start;
+
+        if (start <= 9 && end <= 9) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%01d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%01d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%01d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%01d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start <= 99 && end <= 99) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%02d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%02d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%02d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%02d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start <= 999 && end <= 999) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%03d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%03d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%03d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%03d ", temp);
+                    temp--;
+                }
+            }
+        }
+
+        scan.close();
+
+    }
+}
