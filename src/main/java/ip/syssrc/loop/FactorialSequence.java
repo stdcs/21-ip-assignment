b8202515ea8eb1d4d851a54e210bb42e523088e5
MichaelGabriel01 <arknet31@gmail.com>
2021-10-27 21:21:22 +0800

    feat(loop\FactorialSequence) : add 'java.util.Scanner' object

--- src/main/java/ip/syssrc/loop/FactorialSequence.java
+++ src/main/java/ip/syssrc/loop/FactorialSequence.java
@@ -1,16 +1,40 @@
 package ip.syssrc.loop;
-
+import java.util.Scanner;
 /**
  * FactorialSequence
  *
  * Assignment 2.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *  @author H071211068 - Michael Gabriel Bida <michaelgabriel663@gmail.com>
  *
  */
 public class FactorialSequence {
 
     public static void main(String[] args) {
 
-    }
+        Scanner scan = new Scanner(System.in);
+
+        long fact = 1;
+        int n = scan.nextInt();
+
+        scan.close();
+    
+        if (n > 0) {
+            System.out.print(n + "! = ");
+
+            for (int i = n; i >= 2; i--) {
+                System.out.print(i + " x ");
+                fact = fact * i;
+            }
+
+            System.out.print("1 = " + fact);
+
+        } else if (n == 0) {
+            System.out.print(n + "! = ");
+
+            System.out.println("1");
+        } else {
+            System.out.println("Error");
+        }
+    }   
 }
