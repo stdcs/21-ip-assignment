97644b15b46cb65a55bdf9fa6f00130c520b9c65
Liska <rombeliska@gmail.com>
2021-10-28 00:22:53 +0800

    feat (loop/Eyes):add code2

---
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -0,0 +1,107 @@
+package ip.syssrc.loop;
+
+/**
+ * MatricesPad
+ *
+ * Assignment 2.1
+ *
+ * @author H071211083 - Liska Dewi Rombe <rombeliska@gmail.com>
+ *
+ */
+
+public class MatricesPad {
+
+    public static void main(String[] args) {
+        Scanner lio = new Scanner(System.in);
+
+        int start = lio.nextInt();
+        int end = lio.nextInt();
+        int size = lio.nextInt();
+
+        int range = end - start;
+        int temp = start;
+
+        if (start <= 9 && end <= 9) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%01d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%01d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%01d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%01d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start <= 99 && end <= 99) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%02d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%02d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%02d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%02d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start <= 999 && end <= 999) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%03d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%03d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%03d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%03d ", temp);
+                    temp--;
+                }
+            }
+        }
+
+        lio.close();
+    }
+    
+    }
