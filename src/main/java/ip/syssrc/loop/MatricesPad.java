5ec4f7d6ddf6d39ebea4e232e158c80d496c1b93
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-10-27 14:43:30 +0700

    feature(loop/Eyes): Add nested for and if else statement

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -8,10 +8,24 @@ package ip.syssrc.loop;
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
  *
  */
-
+import java.util.Scanner;
 public class MatricesPad {
 
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(Systemin);
 
+        int start;
+        int end;
+        int i;
+        int size;
+        System.out.print("Start (Bilangan Bulat >= 0 ) = ");
+        start = sc.nextInt();
+        System.out.print("End (Bilangan Bulat >= 0 ) = ");
+        end = sc.nextInt();
+        System.out.print("Size (Bilangan Bulat >= 0 ) = ");
+        size = sc.nextInt();
+        
+        i = start;
     }
 }
