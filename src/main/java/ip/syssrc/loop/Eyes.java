7eb5c6e86096e914421c37b758b1c0e596228a80
stalyaardani <stalyaardaniaries@gmail.com>
2021-10-27 21:39:07 +0800

    feat (loop/Eyes) : add for (int i = 1; i <= n; i++) code and if if (i == j) code in it

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -14,7 +14,14 @@ public class Eyes {
         Scanner in = new Scanner (System.in);
         int n = in.nextInt();
 
-        
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }   System.out.println();
+        } 
     }
 }
