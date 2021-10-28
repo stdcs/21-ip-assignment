62a1364a930e2d91e662900637e1a94045cceec0
Febi Fiantika <febifian23@gmail.com>
2021-10-27 21:38:06 -0700

    fix (conditional/Cashier): add 'changecode' object

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -15,30 +15,30 @@ public class RoleAccess {
         int subMenuIndex = 0;
         boolean isValidRole = false;
 
-    if (args.length == 0){
+    if (args.length == 0) {
         System.out.println("What role you want to see ?");
         System.out.println("For example, try 'Admin'.");
-    }else if(args.length == 1){
-      switch (args[0]) {
-          case "Super Admin":
-          case "Admin":
-          case "User":
-          isValidRole = true;
-          break;
-      }
-      if(isValidRole == true){
-          switch (args[0] ){
+    } else if (args.length == 1) {
+          switch (args[0]) {
               case "Super Admin":
-                menuIndex = menuIndex + 1;
-                System.out.printf("%d. Super Admin\n", menuIndex);
-                subMenuIndex = subMenuIndex + 1;
-                System.out.printf("%2d.%d. CRUD Super Admin\n", menuIndex, subMenuIndex);
-                subMenuIndex = subMenuIndex + 1;
-                System.out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex);
-                subMenuIndex = subMenuIndex + 1;
-                System.out.printf("%2d.%d. CRUD User \n", menuIndex, subMenuIndex);
-                System.out.println();
               case "Admin":
+              case "User":
+                  isValidRole = true;
+                  break;
+      }
+      if (isValidRole == true) {
+          switch (args[0] ) {
+            case "Super Admin":
+              menuIndex = menuIndex + 1;
+              System.out.printf("%d. Super Admin\n", menuIndex);
+              subMenuIndex = subMenuIndex + 1;
+              System.out.printf("%2d.%d. CRUD Super Admin\n", menuIndex, subMenuIndex);
+              subMenuIndex = subMenuIndex + 1;
+              System.out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex);
+              subMenuIndex = subMenuIndex + 1;
+              System.out.printf("%2d.%d. CRUD User \n", menuIndex, subMenuIndex);
+               System.out.println();
+            case "Admin":
                 menuIndex = menuIndex + 1;
                 subMenuIndex = 0;
                 System.out.printf("%d. Admin\n", menuIndex);
