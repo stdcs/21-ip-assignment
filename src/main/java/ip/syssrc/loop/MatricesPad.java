6652dc6fad7403ff739f4e15d9a03337b4f810a6
Djulizah <djul.b74123@gmail.com>
2021-10-30 08:38:29 +0800

    fix(loop/MatricesPad): fix paddingSize

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -16,7 +16,7 @@ public class MatricesPad {
 
         Scanner In = new Scanner(System.in);
 
-        int start, end, size;
+        int start, end, size, paddingSize;
         
 
         System.out.print("start : ");
@@ -31,85 +31,42 @@ public class MatricesPad {
         int range = end - start;
         int temp = start;
 
-        if (start <= 9 && end <= 9) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp--;
-                }
+        //PaddingDigit
+        if (end > start) {
+            for (paddingSize = 1; paddingSize <= end; paddingSize++) {
+            end = end/10;
             }
-        } else if (start <= 99 && end <= 99) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 999 && end <= 999) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp--;
-                }
+        } else {
+            for (paddingSize = 1; paddingSize <= start; paddingSize++) {
+                start = start/10;
             }
         }
-
+        
+        
+        if (range >= 0) {
+            for (int i = 1; i <= range / size; i++) {
+                for (int j = 1; j <= size; j++) {
+                    System.out.printf("%0" + paddingSize + "d ", temp);
+                    temp++;
+                }
+                System.out.println("");
+            }
+            for (int k = 0; k <= range % size; k++) {
+                System.out.printf("%0" + paddingSize + "d ", temp);
+                temp++;
+            }
+        } else if (range <= 0) {
+            for (int i = 1; i <= -range / size; i++) {
+                for (int j = 1; j <= size; j++) {
+                    System.out.printf("%0" + paddingSize + "d ", temp);
+                    temp--;
+                }
+                System.out.println("");
+            }
+            for (int k = 0; k <= -range % size; k++) {
+                System.out.printf("%0" + paddingSize + "d ", temp);
+                temp--;
+            }
+        }
     }
 }
