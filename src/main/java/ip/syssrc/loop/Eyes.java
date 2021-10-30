e86ef49260cb50a54db61424e908e2c7e52ef25c
Djulizah <djul.b74123@gmail.com>
2021-10-30 08:37:39 +0800

    fix(loop/eyes): print format

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -24,12 +24,8 @@ public class Eyes {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 
-                if(i == j ) {
-                    System.out.print(1);
-                } else
-                    System.out.print(0);
+                System.out.printf("%d%s", (i == j)? 1 : 0, (j < n)? " " : "\n" );
             }
-            System.out.println();
         }
     }
 }
