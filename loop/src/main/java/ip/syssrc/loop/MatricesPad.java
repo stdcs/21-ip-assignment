020084af1c482d5ddcf35cc5ef83c226233ae91f
Febi Fiantika <febifian23@gmail.com>
2021-10-30 22:28:03 -0700

    feat(loop/MatricesPad): add 'changecode' object

--- loop/src/main/java/ip/syssrc/loop/MatricesPad.java
+++ loop/src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -19,87 +19,27 @@ public class MatricesPad {
         int size = in.nextInt();
         in.close();
 
-        int range = end - start;
-        int temp = start;
+        int biggest = (start > end) ? start : end;
+        int leadingZero = 0;
+        while (biggest > 0) {
+            biggest /= 10;
+            leadingZero++;
+        }
 
-        if (start <= 9 && end <= 9) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp--;
-                }
+        /** OUTPUT */
+        for (int i = start; (start < end ? i <= end : i >= end); ) {
+            if (((i - start) % size == 0 || (start - i) % size == 0) 
+                    && (i != start)) {
+                System.out.printf("\n");
             }
-        } else if (start <= 99 && end <= 99) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 999 && end <= 999) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp--;
-                }
+
+            System.out.printf("%0" + leadingZero + "d ", i);
+            if (start < end) {
+                i++;
+            } else {
+                i--;
             }
         }
+
     }
 }
