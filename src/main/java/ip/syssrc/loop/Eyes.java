04f809b60687c58b104b4f61c2a68873271003fd
Liska <rombeliska@gmail.com>
2021-10-31 21:22:18 +0800

    fix (loop/Eyes): add change

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -15,16 +15,20 @@ public class Eyes {
 
         int n = li.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
-                    System.out.print(1 + " ");
-                } else {
-                    System.out.print(0 + " ");
-                }
-            }
-            System.out.println();
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++){
+                    System.out.printf("%d%s", (i == j) ? 1 :0, (j <= n) ? " " : "\n");
+                System.out.printf("%d%s", (i == j) ? 1 : 0, (j <= n) ? " " : "\n");
+        
         }
-    }
+        
+        System.out.println();
     
+     }
+     
+    }
 }
+
+
+        
+    
