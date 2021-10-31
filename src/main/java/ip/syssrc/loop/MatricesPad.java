a30600142d5e67e98b84f2c2d25d1a6d995cffd6
Muhammad Faiz Fatwa Syarifuddin <92856595+faizftw@users.noreply.github.com>
2021-10-31 12:53:36 +0800

    fix(loop/MadtricePad)
    mengganti kode untuk leading zero, mengganti kode agar bisa melakukan padding tanpa batas angka

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -22,86 +22,47 @@ public class MatricesPad {
 
         int range = end - start;
         int temp = start;
+        int largest;
+        int leadingZero = 0;
 
-        if (start <= 9 && end <= 9) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 99 && end <= 99) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 999 && end <= 999) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp--;
-                }
-            }
+        if (start>end) {
+            largest = start;
+        } else {
+            largest = end;
         }
+        
+        while (largest > 0) {
+            largest /= 10;
+            leadingZero++;
+        }
+
+        if (start >= 0 && end >= 0) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%0" + leadingZero + "d ", temp);
+                        temp++;
+                    }
+                    System.out.printf("%n");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%0" + leadingZero + "d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%0" + leadingZero + "d ", temp);
+                        temp--;
+                    }
+                    System.out.printf("%n");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%0" + leadingZero + "d ", temp);
+                    temp--;
+                }
+            }
+        } 
         in.close();
     }
 }
