5b0a65f95b51a7c715bceeaab40b858a87834a1a
AndiRajaZulfaqar <andiraja954@gmail.com>
2021-11-01 15:21:51 +0100

    feat(Array/CountEach): add 'java.util.Scanner' object

---
+++ src/main/java/ip/syssrc/Array/CountEach.java
@@ -0,0 +1,31 @@
+package ip.syssrc.Array;
+
+import java.util.Scanner;
+
+/**
+ * CountEach
+ *
+ * Assignment 1.1
+ *
+ * @author H071211057 - AndiRajaZulfaqar <andiraja954@gmail.com>
+ *
+ */
+public class CountEach {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = in.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        in.close();
+    }
+}
---
+++ src/main/java/ip/syssrc/Array/SumOneThird.java
@@ -0,0 +1,49 @@
+package ip.syssrc.Array;
+
+import java.util.Scanner;
+
+/**
+ * CountEach
+ *
+ * Assignment 1.1
+ *
+ * @author H071211057 - AndiRajaZulfaqar <andiraja954@gmail.com>
+ *
+ */
+public class SumOneThird {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sum;
+        int n = in.nextInt();
+        int aritmatika = (n/3);
+        int[] array = new int[n];
+        for (int i=0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
+
+        for(int i = 0; i < n/3; i++) {
+            sum = 0;
+            for (int j = 0; j < 3; j++ ) {
+                int deret = i + (j * aritmatika);
+                sum = sum + array[deret];
+                if (j == 2) {
+                    System.out.println(array[deret] + " = " + sum);;
+                } else {
+                    System.out.print(array[deret] + " + ");
+                }
+            }
+        }
+        if (n%3 > 0) {
+            sum = 0;
+            for (int i = ((n/3) * 3); i < n; i++) {
+                sum = sum + array[i];
+                if (i == n-1) {
+                    System.out.print(array[i] + " = " + sum);
+                } else {
+                    System.out.print(array[i] + " + ");
+                }
+            }
+        }
+        in.close();
+    }
+}
