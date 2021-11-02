8778a157ddb964e496f7fa21b549ebf95dfa0a93
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-11-02 20:01:53 +0700

    fix(loop/Eyes) : import java.util.Scanner;

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -15,18 +15,11 @@ public class Eyes {
 
         int n = in.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i=0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (i == j) {
-                    System.out.print(1 + " ");
-
-                } else {
-                    System.out.print(0 + " ");
-                }
-            }
-            System.out.println();
-        }
-
-        in.close();
+                    System.out.print("1 ");
+                
     }
 }
+
