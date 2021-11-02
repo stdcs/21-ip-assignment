f11c47f3b07f978b5fa386a283cbdfe059fd2be6
FathurRachmanAlamsyah1041 <sembarangm135@gmail.com>
2021-11-02 09:46:10 +0800

    feat(loop):fix `MatricesPad.java`

--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -6,10 +6,14 @@ package ip.syssrc.loop;
  * Assignment 2.1
  *
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @author H071211041 - Fathur Rachman Alamsyag <sembarangm135@gmail.com>
 =======
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
 >>>>>>> 3455b10 (feat(loop): add assignment)
+=======
+ * @author H071211041 - Fathur Rachman Alamsyag <sembarangm135@gmail.com>
+>>>>>>> 2d0a2cf (feat(loop):fix)
  *
  */
 import java.util.Scanner;
@@ -20,6 +24,7 @@ public class MatricesPad {
 
         Scanner t2 = new Scanner(System.in);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
         System.out.println("Input Angka Awal, Akhir, dan Ukuran (Max is Integer Value '2147483647')");
         int start = t2.nextInt();
@@ -105,94 +110,93 @@ public class MatricesPad {
     }
 }
 =======
+=======
+        System.out.println("Input Angka Awal, Akhir, dan Ukuran (Max is Integer Value '2147483647')");
+>>>>>>> 2d0a2cf (feat(loop):fix)
         int start = t2.nextInt();
         int end = t2.nextInt();
         int size = t2.nextInt();
-
-        int range = end - start;
-        int temp = start;
-
-        if (start <= 9 && end <= 9) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp++;
+        int awal = start;
+        if (end >= start) {
+            while (end >= start) {
+                for (int i = 0; i < size; i++) {
+                    if (start == end) {
+                        break;
                     }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%01d ", temp);
-                        temp--;
+                    if (start < 10) {
+                        System.out.print("0" + start + " ");
+                    } else {
+                        System.out.print(start + " ");
                     }
-                    System.out.println("");
+                    start++;
                 }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%01d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 99 && end <= 99) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp++;
+                if (start == end) {
+                    if ((awal - end) % size == 0) {
+                        if (start < 10) {
+                            System.out.println("");
+                            System.out.println("0" + start);
+                            break;
+                        } else {
+                            System.out.println("");
+                            System.out.println(start);
+                            break;
+                        }
+                    } else {
+                        if (start < 10) {
+                            System.out.println("0" + start);
+                            break;
+                        } else {
+                            System.out.println(start);
+                            break;
+                        }
                     }
-                    System.out.println("");
                 }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%02d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%02d ", temp);
-                    temp--;
-                }
-            }
-        } else if (start <= 999 && end <= 999) {
-            if (range >= 0) {
-                for (int i = 1; i <= range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp++;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp++;
-                }
-            } else if (range <= 0) {
-                for (int i = 1; i <= -range / size; i++) {
-                    for (int j = 1; j <= size; j++) {
-                        System.out.printf("%03d ", temp);
-                        temp--;
-                    }
-                    System.out.println("");
-                }
-                for (int k = 0; k <= -range % size; k++) {
-                    System.out.printf("%03d ", temp);
-                    temp--;
-                }
+                System.out.println();
             }
         }
 
+        else {
+            while (start >= end) {
+                for (int i = 0; i < size; i++) {
+                    if (start == end) {
+                        break;
+                    }
+                    if (start < 10) {
+                        System.out.print("0" + start + " ");
+                    } else {
+                        System.out.print(start + " ");
+                    }
+                    start--;
+                }
+                if (start == end) {
+                    if ((awal - end) % size == 0) {
+                        if (start < 10) {
+                            System.out.println("");
+                            System.out.println("0" + start);
+                            break;
+                        } else {
+                            System.out.println("");
+                            System.out.println(start);
+                            break;
+                        }
+                    } else {
+                        if (start < 10) {
+                            System.out.println("0" + start);
+                            break;
+                        } else {
+                            System.out.println(start);
+                            break;
+                        }
+                    }
+                }
+                System.out.println();
+            }
+        }
         t2.close();
     }
+<<<<<<< HEAD
 }
 >>>>>>> 3455b10 (feat(loop): add assignment)
+=======
+}
+>>>>>>> 2d0a2cf (feat(loop):fix)
