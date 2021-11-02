38862cea80fed5285fd92a5f575ea22a5197d25f
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-02 13:09:14 +0800

    fix (loop/Eyes) : fix the program

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -14,14 +14,15 @@ public class Eyes {
         Scanner in = new Scanner (System.in);
         int n = in.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (i == j) {
-                    System.out.print(1 + " ");
+                    System.out.print("1 ");
                 } else {
-                    System.out.print(0 + " ");
+                    System.out.print("0 ");
                 }
-            }   System.out.println();
+            }
+            System.out.println();
         }
         in.close();
     }
