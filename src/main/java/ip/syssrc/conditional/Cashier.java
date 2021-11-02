6b5563a84d5cd123e58c5efeb5d2fd92a4ab8ee2
FathurRachmanAlamsyah1041 <sembarangm135@gmail.com>
2021-11-02 10:45:43 +0800

    feat(conditional): Add Tugas 1 `Cashier.java` `RoleAccess.java`

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -1,15 +1,15 @@
 package ip.syssrc.conditional;
 
-import java.util.Scanner;
-
 /**
  * Cashier
  *
  * Assignment 1.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211041 - Fathur Rachman Alamsyah <sembarangm135@gmail.com>
  *
  */
+import java.util.Scanner;
+
 public class Cashier {
 
     public static void main(String[] args) {
