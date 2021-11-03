3527a0f931ff991b10ce0b22ee0a145a39a3737c
AndiMuhIbnuHibbanBAgoesMalolo <andibagoes1412@gmail.com>
2021-11-03 18:52:41 +0800

    feat(loop/Eyes): add format 'for' and 'if'

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -14,12 +14,12 @@ public class Eyes {
         Scanner obj = new Scanner(System.in);
         int n = obj.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i=0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (i == j) {
-                    System.out.print(1 + " ");
+                    System.out.print("1 ");
                 } else {
-                    System.out.print(0 + " ");
+                    System.out.print("0 ");
                 }
             }
             System.out.println();
