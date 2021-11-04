bd95af8cbfd83d240033a98dabcb2956c3eb369a
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-11-04 22:50:05 +0800

    feat(array/SumOneThird): add code

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,16 +5,45 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211074 - Wd. Ananda Lesmono <nandalesmono@gmail.com>
  *
  */
 public class SumOneThird {
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 100 };
 
+        
         for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+            int sum = 0;
+            for (int j = 0; j < 3; j++ ) {
+                int aritmatika = arr.length / 3;
+                int deret = index + (j * aritmatika);
+                sum = sum + arr[deret];
+                if (j == 2) {
+                    System.out.println(arr[deret] + " = " + sum);;
+                } else {
+                    System.out.print(arr[deret] + " + ");
+                }
+            }
         }
+        if (arr.length % 3 > 0) {
+            int sum = 0;
+            for (int index = ((arr.length / 3) * 3); index < arr.length; index++) {
+                sum = sum + arr[index];
+                if (index == arr.length - 1) {
+                    System.out.print(arr[index] + " = " + sum);
+                } else {
+                    System.out.print(arr[index] + " + ");
+                }
+            }
+        } 
+        System.out.print("\nsum of ");
+        int sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            System.out.print("[" + arr[index] + "]");
+            sum += arr[index];
+        }
+        System.out.println(" = " + sum);
     }
 }
