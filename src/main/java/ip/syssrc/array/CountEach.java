131217aa104fd38954a825de8e059ee8ed38f878
sofyanox12 <gaero38@gmail.com>
2021-11-05 00:55:37 +0900

    Assignment 3 - H071211045

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -1,17 +1,65 @@
 package ip.syssrc.array;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * CountEach
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211045 - MUHAMMAD SOFYAN DAUD PUJAS <gaero38@gmail.com>
  *
  */
-
 public class CountEach {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] occurence;
+        int[] index;
+        int size;
 
+        // Interface-Input
+        System.out.print("\nMasukkan banyaknya angka\nyang Ingin di-input : ");
+        try { // Filtering input
+            size = sc.nextInt();
+            index = new int[size];
+            System.out.print("\nMasukkan angka yang dimaksud (" + size + "): ");
+            for (int i = 0; i < size; i++) {
+                index[i] = sc.nextInt();
+            }
+            sc.close();
+        } catch (InputMismatchException e) {
+            System.out.println("Mohon masukkan angka!\n");
+            return;
+        }
+
+        // Intialize pre-statements
+        occurence = new int[size];
+        int opt = 1;
+        int fit = -1;
+
+        // Find the exact same numbers
+        for (int i = 0; i < size; i++) {
+            for (int t = i + 1; t < size; t++) {
+                if (index[i] == index[t]) {
+                    occurence[t] = fit;
+                    ++opt;
+                }
+            }
+            if (occurence[i] != fit) {
+                occurence[i] = opt;
+            }
+        }
+
+        // Prints the result
+        System.out.println("Terdapat : ");
+        for (int i = 0; i < size; i++) {
+            if (occurence[i] != fit) {
+                System.out.print( occurence[i] + " Angka " + index[i] + "\n");
+            }
+        }
+             
+        
+        
     }
 }
--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,16 +5,67 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211045 - MUHAMMAD SOFYAN DAUD PUJAS <H071211045>
  *
  */
 public class SumOneThird {
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
-        for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+        // First statements
+        int[] arr = { 55, 60, 75, 80, 95, 100, 95, 80, 75, 60, 55, 45 };
+        int length = arr[arr.length - 1];
+        int arrangedArray;
+        int arrayIndex = 0;
+        int arrayTotal = 0;
+        int preTotal = 0;
+        int opt = 0;
+    
+        // Sorting out array
+        for (int i = 0; i < arr.length; i++) {
+            
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    arrangedArray = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = arrangedArray;
+                }
+            }
         }
+
+        // Align the array by the right side
+        while (length > 0) {
+            length /= 10;
+            ++opt;
+        }
+        
+        // Array operation
+        for (int i = 0; i < (arr.length / 3); i++) {
+            int oneThird = (arr.length / 3);
+            arrayIndex = i; // Return the arrayIndex to become i for every increment
+            for (int t = 0; t < 3; t++) {
+                preTotal += arr[arrayIndex];
+                System.out.printf("%s%" + opt + "d", (arrayIndex != i) ? " + " : "", arr[arrayIndex]);
+                arrayIndex += oneThird;
+            }
+            arrayTotal += preTotal;
+            System.out.print(" = " + preTotal + "\n");
+
+            if ((i + 1) == oneThird) { // The possible outcome calculations for every index
+                preTotal = 0;
+                for (int t = oneThird * 3; t < arr.length; t++) {
+                    preTotal += arr[t];
+                    System.out.printf("%" + opt + "d%s", arr[t], (t != arr.length - 1) ? " + " : " = " + preTotal + "\n");
+                }
+                System.out.print("\n");
+                arrayTotal += preTotal;
+            }
+        }
+
+        // Result
+        System.out.print("Sum of ");
+        for (int i = 1; i < arr.length; i++) { // Showing every array index by print out
+            System.out.print("[" + i + "]");
+        }
+        System.out.print(" = " + arrayTotal);
     }
 }
