751205375d9336fa5adb1691b36925ba7050206e
SyifaUrRahmi <sifasisi3@gmail.com>
2021-11-04 22:47:54 +0800

    feat(array/SumOneThird) : import java.util.Scanner

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -8,9 +8,12 @@ package ip.syssrc.array;
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
  *
  */
+import java.util.Scanner;
 public class SumOneThird {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
         for (int index = 0; index < (arr.length / 3); index++) {
