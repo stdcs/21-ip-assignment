2aa2e0e5cc7893658e1479ae40dbf44e6c34a6b0
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-04 15:37:47 +0800

    feat (array/RadixConverter) : add 'import java.util.Scanner' object, variable integer, and variable array integer

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -1,16 +1,29 @@
 package ip.syssrc.array;
-
+import java.util.Scanner;
 /**
  * RadixConverter
  *
  * Assignment 3.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211061 - St. Alya Ardani Aries <stalyaardaniaries@gmail.com>
  *
  */
 public class RadixConverter {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int a, b, c;
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
 
+        int[] twoMultiplier = new int[8];
+        int[] eightMultiplier = new int[8];
+        int[] sixTeenMultiplier = new int[8];
+        int[] base2 = new int[8];
+        int[] base8 = new int[8];
+        int[] base16 = new int[8];
+        int sum;
+        
     }
 }
