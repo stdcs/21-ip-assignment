25116cc361e1d8119f5275803ebe8dbf42fe2969
Djulizah <djul.b74123@gmail.com>
2021-11-04 22:02:25 +0800

    feat(array/CountEach): Add java.util.Scanner

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -5,13 +5,51 @@ package ip.syssrc.array;
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211073 - Djulizah Bonita L
  *
  */
-
+import java.util.Scanner;
 public class CountEach {
 
     public static void main(String[] args) {
+        Scanner Obj = new Scanner(System.in);
 
+        int n;
+        System.out.print("Input n = ");
+        n = Obj.nextInt();
+
+        int[] memo = new int [n];
+
+        //looping input until n
+        for (int i = 0 ; i < n; i++ ) {
+            memo [i] = Obj.nextInt();
+        }
+
+        //Array freq will store frequencies of element  
+        int [] freq = new int [memo.length];  
+        int visited = -1;  
+     
+        for(int i = 0; i < memo.length; i++){  
+            int count = 1;  
+            
+            for(int j = i+1; j < memo.length; j++){  
+                if(memo[i] == memo[j]){  
+                    count++;  
+                    
+                    //To avoid counting same element again  
+                    freq[j] = visited;  
+                }  
+            }  
+            
+            if(freq[i] != visited) {
+             freq[i] = count;  
+            }  
+        
+            if(freq[i] != visited) {  
+                System.out.println(freq[i] + " Angka " + memo[i]);  
+            }
+
+        }
+        Obj.close();
     }
 }
