6907bd20ab98db8aa56a94352a7b111337c1af27
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-11-04 21:07:20 +0700

    feature(array/RadixConverter): Initialize Variables, Add Scanner Object,If Else Statement and, For Looping

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -5,12 +5,41 @@ package ip.syssrc.array;
  *
  * Assignment 3.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211064 - Agung Kartika Ardhiyanda  <agungardhiyanda25@gmail.com>
  *
  */
+import java.util.Scanner;
 public class RadixConverter {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a,i,desimal,hasilbagi;
+        int[]b;
+        int[]pembagi = {2, 8, 16 };
+        
+        a = sc.nextInt();
+        sc.close();
+
+        for(i = 0; i < divider.length; i++){
+            b = new int[8];
+            desimal = a;
+            System.out.printf("Base %2d: ", pembagi[i]);
+            for(int j = (b.length - 1); decimal > 0; j--) {
+                hasilbagi = (decimal % pembagi[i]);
+                b[j] = (hasilbagi > 9) ? (hasilbagi + 55) : hasilbagi;
+                desimal /= pembagi[i];
+            }
+            for (int z : b){
+                if(z > 9){
+                    System.out.printf("%c", (char) d);
+                } else {
+                    System.out.printf("%d", d);
+                }
+            }
+            System.out.printf("%s", (i !=pembagi.length -1) ? "\n" : "");
+        }
 
     }
 }
