bf714e80d006be1508eaca3721a38dfab8a3f995
Hafieddz <hasmy41@gmail.com>
2021-11-05 03:31:33 +0800

    feat(array/RadixConverter.java): add variabel

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -12,6 +12,13 @@ public class RadixConverter {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        int m = n;
+        int o = n;
+        int [] twoMultiplier = new int[8];
+        int [] eightMultiplier = new int[8];
+        int [] sixteenMultiplier = new int[8];
+        int sum; 
+
     }
 }
