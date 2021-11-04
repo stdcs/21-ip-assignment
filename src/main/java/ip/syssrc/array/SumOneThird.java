5135d2fa1012b8769905a7eaf38927b661960aa7
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-11-04 22:55:10 +0700

    feature(array/SumOneThird): Add for looping, if else statement with ternary operator

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -12,9 +12,35 @@ public class SumOneThird {
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
+        
         for (int index = 0; index < (arr.length / 3); index++) {
             // Your magic is here
+            int oneThird = arr.length / 3;
+            int jumlah = 0;
+            int i = index;
+            int spasi = 0;
+            int angkaTerbesar = arr[arr.length -1];
+
+            while (angkaTerbesar > 0){
+                angkaTerbesar /= 10;
+                spasi++;
+            }
+
+            for (int j = 0; j <3; j++){
+                jumlah += arr[i];
+                System.out.printf("%s%"+spasi+"d", (i!=index)?"+":"",arr[i]);
+                i +=oneThird;
+            }
+            System.out.printf("%d\n", jumlah);
+
+            if (index ==(oneThird - 1)){
+                jumlah = 0;
+                for(int k = oneThird * 3; k < arr.length; k++){
+                    sjumlah += arr[j];
+                    System.out.printf("%"+spasi+"d%s",arr[k],(j!=arr.length - 1)) ? "+":"="+jumlah+"\n");
+                }
+            }
+            }
         }
     }
 }
