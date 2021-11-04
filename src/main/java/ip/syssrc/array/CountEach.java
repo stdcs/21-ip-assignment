4fba450b8c983c3417f0ad1b5c6c0adcbc084396
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-11-04 22:48:00 +0800

    feat(array/CountEach): add code

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -5,13 +5,24 @@ package ip.syssrc.array;
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211074 - Wd. Ananda Lesmono <nandalesmono@gmail.com>
  *
  */
-
+import java.util.Scanner;
 public class CountEach {
 
     public static void main(String[] args) {
-
+        Scanner dede = new Scanner(System.in);
+        int n = dede.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = in.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
     }
 }
