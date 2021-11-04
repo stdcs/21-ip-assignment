a0bd771dc6d82d4b66f079b28c8074d3666a561e
SyifaUrRahmi <sifasisi3@gmail.com>
2021-11-04 22:39:39 +0800

    feat(array/RadixConverter) : import java.util.Scanner

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -8,9 +8,10 @@ package ip.syssrc.array;
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
  *
  */
+import java.util.Scanner;
 public class RadixConverter {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
     }
 }
