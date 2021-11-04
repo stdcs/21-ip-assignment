985b09a226f53a79620f15b4321368ee8b4de0a1
Hafieddz <hasmy41@gmail.com>
2021-11-05 02:57:52 +0800

    feat(array/SumOneThird.java): add 'for loop' condition

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,16 +5,21 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
  *
  */
 public class SumOneThird {
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
+        int sum = 0;
         for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+            sum = 0;
+            for (int j = 0; j < 3; j++) {
+                int deret = index + (j * (arr.length/3));
+                sum = sum + arr[deret];
+            }
         }
+        
     }
 }
