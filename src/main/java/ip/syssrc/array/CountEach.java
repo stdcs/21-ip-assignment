f70c8830565b2dfa5bd9fa47ad9a465ddceca101
Hafieddz <hasmy41@gmail.com>
2021-11-05 03:17:38 +0800

    feat(array/CountEach.java): add 'java.util.Scanner' object

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -1,17 +1,18 @@
 package ip.syssrc.array;
-
+import java.util.Scanner;
 /**
  * CountEach
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211085 - Hafiedz Hasmy Hamid <hasmy41@gmail.com>
  *
  */
 
 public class CountEach {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        
     }
 }
