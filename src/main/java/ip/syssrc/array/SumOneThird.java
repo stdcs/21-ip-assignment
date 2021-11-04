78a57cb35dff939a10ebb3660229686c3800d44f
syaarifhd <syarifdayat119@gmail.com>
2021-11-04 22:57:37 +0800

    feat(array): add assignment CountEach.java RadixConverter.java SumOneThird.java

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,7 +5,7 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211087 - Syarif Hidayat <syarifdayat119@gmail.com>
  *
  */
 public class SumOneThird {
@@ -16,5 +16,31 @@ public class SumOneThird {
         for (int index = 0; index < (arr.length / 3); index++) {
             // Your magic is here
         }
+        for(int i =0; i<n/3; i++) {
+            sum = 0;
+            for (int j =0; j<3; j++) {
+                int deret = i + (j * aritmatika);
+                sum = sum + array[deret];
+                if (j == 2){
+                    System.out.println(array[deret] + "=" + sum);
+                }else{
+                    System.out.print(array[deret] + "+");
+
+                }
+            }
+        }
+            if (n%3 > 0){
+                sum = 0;
+                for (int i = ((n/3) * 3); i<n; i++){
+                    sum = sum + array[i];
+                    if (i == n-1){
+                        System.out.print(array[i] + " = " + sum);
+                    }else{
+                        System.out.print(array[i] + "+");
+
+                    }
+                    in.close();
+                }
+            }
     }
 }
