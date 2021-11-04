c87703b018cb5e046496829b6462b31cd0b213eb
FjrSdq <fsiddiq072@gmail.com>
2021-11-04 22:33:54 +0800

    feat(array) add Tugas Praktikum 3

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -1,17 +1,29 @@
 package ip.syssrc.array;
-
+import java.util.Scanner;
 /**
  * CountEach
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211078 - Muh Fajar Siddiq <fsiddiq072@gmail.com>
  *
  */
-
 public class CountEach {
-
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = in.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
 
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        in.close();
     }
 }
