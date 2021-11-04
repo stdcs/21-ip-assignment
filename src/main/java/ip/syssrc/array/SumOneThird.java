3d1b04c45da8c814f93df891590a1b383ab0ddbc
Liska <rombeliska@gmail.com>
2021-11-05 00:31:52 +0800

    feat (array/SumOneThird): add code

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,7 +5,7 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211083 - liska Dewi Rombe <rombeliska@gmail.com>
  *
  */
 public class SumOneThird {
@@ -14,7 +14,29 @@ public class SumOneThird {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
         for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+            sum = 0;
+            for (int j = 0; j < 3; j++ ) {
+                int deret = i + (j * aritmatika);
+                sum = sum + array[deret];
+                if (j == 2) {
+                    System.out.println(array[deret] + " = " + sum);;
+                } else {
+                    System.out.print(array[deret] + " + ");
+                }
+            }
         }
+        if (n%3 > 0) {
+            sum = 0;
+            for (int i = ((n/3) * 3); i < n; i++) {
+                sum = sum + array[i];
+                if (i == n-1) {
+                    System.out.print(array[i] + " = " + sum);
+                } else {
+                    System.out.print(array[i] + " + ");
+                }
+            }
+        }
+        liska.close();
     }
-}
+
+    }
