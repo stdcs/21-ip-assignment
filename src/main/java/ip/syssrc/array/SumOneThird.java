f9d733815c97a100847685d33ba10148f5d78b0d
Mhmmdfhkri <mufik2002@gmail.com>
2021-11-04 06:47:05 -0700

    feat(Array/Tugas3):add 'java.util.Scanner' Object

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,16 +5,44 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211056 - Muhammad Fikri <mufik2002@gmail.com>
  *
  */
 public class SumOneThird {
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
-        for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+        Scanner in = new Scanner(System.in);
+        int sum;
+        int n = in.nextInt();
+        int aritmatika = (n/3);
+        int[] array = new int[n];
+        for (int i=0; i < array.length; i++) {
+            array[i] = in.nextInt();
         }
+
+        for(int i = 0; i < n/3; i++) {
+            sum = 0;
+            for (int j = 0; j < 3; j++ ) {
+                int deret = i + (j * aritmatika);
+                sum = sum + array[deret];
+                if (j == 2) {
+                    System.out.println(array[deret] + " = " + sum);;
+                } else {
+                    System.out.print(array[deret] + " + ");
+                }
+            }
+        }
+        if (n%3 > 0) {
+            sum = 0;
+            for (int i = ((n/3) * 3); i < n; i++) {
+                sum = sum + array[i];
+                if (i == n-1) {
+                    System.out.print(array[i] + " = " + sum);
+                } else {
+                    System.out.print(array[i] + " + ");
+                }
+            }
+        }
+        in.close();
     }
 }
