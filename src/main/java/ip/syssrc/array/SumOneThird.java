c0fab4177315b79cd665f429b6b3d12483e3365c
erwinarif31 <muherwinarif31@gmail.com>
2021-11-04 23:42:39 +0800

    feat(array/SumOneThird): add logic to calculate sum of array

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -15,6 +15,42 @@ public class SumOneThird {
 
         for (int index = 0; index < (arr.length / 3); index++) {
             // Your magic is here
+            int oneThirdOfArr = arr.length / 3;
+            int sum = 0;
+            int i = index;
+
+            /** Menghitung jumlah digit terbesar */
+            int space = 0;
+            int biggestNumber = arr[arr.length - 1];
+            while (biggestNumber > 0) {
+                biggestNumber /= 10;
+                space++;
+            }
+
+            for (int k = 0; k < 3; k++) {
+                sum += arr[i];
+                System.out.printf("%s%" + space + "d", (i != index) ? " + " : "", arr[i]);
+                i += oneThirdOfArr;
+            }
+
+            System.out.printf(" = %d\n", sum);
+
+            if (index == (oneThirdOfArr - 1)) {
+                sum = 0;
+                for (int j = oneThirdOfArr * 3; j < arr.length; j++) {
+                    sum += arr[j];
+                    System.out.printf("%" + space + "d%s", arr[j], (j != arr.length - 1) ? " + " : " = " + sum + "\n");
+                }
+                System.out.printf("\n");
+
+                System.out.print("Sum of ");
+                sum = 0;
+                for (int k = 0; k < arr.length; k++) {
+                    System.out.printf("[%d]", arr[k]);
+                    sum += arr[k];
+                }
+                System.out.printf(" = %d", sum);
+            }
         }
     }
 }
