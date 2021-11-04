fe9e06a936cbbbec8dcec4eb9021ccf74ad514ff
SyifaUrRahmi <sifasisi3@gmail.com>
2021-11-04 22:33:43 +0800

    feat(array/CountEach) : import java.util.Scanner

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -8,10 +8,10 @@ package ip.syssrc.array;
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
  *
  */
-
+import java.util.Scanner;
 public class CountEach {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
     }
 }
