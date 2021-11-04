1f3e44d09acaeda266eb2b7a3a54acf604581b65
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-04 15:47:18 +0800

    feat (array/RadixConverter) : add the program for base 2, there are for and while codes

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -24,6 +24,27 @@ public class RadixConverter {
         int[] base8 = new int[8];
         int[] base16 = new int[8];
         int sum;
+
+        // Base 2 //
+        for (int i = 0; i < 8; i++) {
+            sum = 1;
+            for (int j = 0; j < i; j++) {
+                sum = sum * 2;
+            }
+            twoMultiplier[i] = sum;
+        }
+
+        while (a >= 1) {
+            for (int i = 7; i >= 0 ; i--) {
+                a = a - twoMultiplier[i];
+                base2[7 - i]++;
+            }
+        }
+        System.out.print("Base  2: ");
+        for (int i = 0; i < twoMultiplier.length; i++) {
+            System.out.print(base2[i]);
+        }
+        System.out.println();
         
     }
 }
