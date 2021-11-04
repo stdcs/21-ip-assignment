5bbd786f393530d763c16b0a67f11f1d4d81cdc8
SyifaUrRahmi <sifasisi3@gmail.com>
2021-11-04 22:50:09 +0800

    feat(array/SumOneThird) : Membuat programnya

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -13,11 +13,34 @@ public class SumOneThird {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
         for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+            int sum = 0;
+            sum += arr[i];
+        }
+        for(int i = 0; i < arr.length / 3; i++) {
+            for (int j = 0; j < 3; j++ ) {
+                int deret = i + (j * (arr.length / 3));
+                sum = sum + arr[deret];
+                if (j == 2) {
+                    System.out.println(arr[deret] + " = " + sum);;
+                } else {
+                    System.out.print(arr[deret] + " + ");
+                }
+            }
+        }
+        if (arr.length%3 > 0) {
+            sum = 0;
+            for (int i = ((arr.length/3) * 3); i < arr.length; i++) {
+                sum = sum + arr[i];
+                if (i == arr.length-1) {
+                    System.out.print(arr[i] + " = " + sum);
+                } else {
+                    System.out.print(arr[i] + " + ");
+                }
+            }
         }
     }
 }
