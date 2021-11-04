61144a0710e8861e19fb1ead45d7fc101f51d94c
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-04 15:04:00 +0800

    feat (array/CountEach) : add import java.util.Scanner and variable integer

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -1,17 +1,20 @@
 package ip.syssrc.array;
-
+import java.util.Scanner;
 /**
  * CountEach
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211061 - St. Alya Ardani Aries <stalyaardaniaries@gmail.com>
  *
  */
 
 public class CountEach {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        int[] array = new int[10000000];
+        
     }
 }
