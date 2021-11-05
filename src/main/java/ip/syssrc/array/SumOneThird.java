e3807c677b5acea6fa29a8ffe402784874f8e6fe
Febi Fiantika <febifian23@gmail.com>
2021-11-05 01:04:08 -0700

    feat(array): add Assignment3

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -18,7 +18,23 @@ public class SumOneThird {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
         for (int index = 0; index < (arr.length / 3); index++) {
-            // Your magic is here
+            sum = 0;
+            for (int j = 0; j < 3; j++ ) {
+                int deret = index + (j * (arr.length/3));
+                sum = sum + arr[deret];
+                if (j == 2) {
+                    System.out.println(arr[deret] + " = " + sum);;
+                } else {
+                    System.out.print(arr[deret] + " + ");
+                }
+            }
+        }
+        System.out.print("Sum of ");
+            for(int i = 0; i < arr.length; i++){
+                    sum += arr[i];
+                    System.out.print("[" + arr[i] + "]");
+            }
+            System.out.println(" = " + sum);
         }
     }
 }
