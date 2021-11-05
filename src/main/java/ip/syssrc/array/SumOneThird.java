555fe35845ef01b88e1ef5ab4ef8af235adb033d
Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
2021-11-05 16:54:37 +0800

    feat(array/SumOneThird): add code

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,16 +5,35 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211065 - Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
  *
  */
+import java.util.Scanner;
 public class SumOneThird {
 
     public static void main(String[] args) {
+        Scanner bk = new Scanner(System.in);
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-
+        int sum = 0;
+        bk.close();
         for (int index = 0; index < (arr.length / 3); index++) {
             // Your magic is here
+            sum = 0;
+            for (int j = 0; j < 3; j++ ) {
+                int deret = index + (j * (arr.length/3));
+                sum = sum + arr[deret];
+                if (j == 2) {
+                    System.out.println(arr[deret] + " = " + sum);;
+                } else {
+                    System.out.print(arr[deret] + " + ");
+                }
+            }
         }
+        System.out.print("Sum of ");
+            for(int i = 0; i < arr.length; i++){
+                    sum += arr[i];
+                    System.out.print("[" + arr[i] + "]");
+            }
+            System.out.println(" = " + sum);
     }
 }
