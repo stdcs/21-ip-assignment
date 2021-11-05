e9846ed893fdf83bf405e38a0fdd3d29573bb895
Febi Fiantika <febifian23@gmail.com>
2021-11-05 01:04:08 -0700

    feat(array): add Assignment3

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -1,5 +1,5 @@
 package ip.syssrc.array;
-
+import java.util.Scanner;
 /**
  * CountEach
  *
@@ -16,6 +16,20 @@ package ip.syssrc.array;
 public class CountEach {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = in.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
 
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        in.close();
     }
 }
