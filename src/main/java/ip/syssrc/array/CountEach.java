756a8561569f37390dfd51e45101b71c9eac163f
DizaAlysha <dizalysha@gmail.com>
2021-11-05 10:26:56 +0800

    feat(array/CountEach): add code

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -5,13 +5,26 @@ package ip.syssrc.array;
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071171512 - Diza Alysha Zahra <dizalysha@gmail.com>
  *
  */
 
-public class CountEach {
+ public class CountEach {
 
     public static void main(String[] args) {
+        Scanner dz = new Scanner(System.in);
+        int n = dz.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = dz.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
 
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
     }
 }
