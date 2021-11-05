ad8aa36dd7a87d2046248eeb797ab61252b06fac
Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
2021-11-05 16:51:23 +0800

    feat(array/CountEach): add code

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -5,13 +5,26 @@ package ip.syssrc.array;
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211065 - Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
  *
  */
-
+import java.util.Scanner;
 public class CountEach {
-
     public static void main(String[] args) {
+        Scanner bk = new Scanner(System.in);
+        int n = bk.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = bk.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
 
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        bk.close();
     }
 }
