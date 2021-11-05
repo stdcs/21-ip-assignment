d06f7a7ca0fbda0ace72593a3f6fd19f6c39129d
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-11-05 14:06:32 +0700

    feat(array/RadixConverter) : import java.util.Scanner;

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -1,16 +1,27 @@
 package ip.syssrc.array;
-
+import java.util.Scanner; 
 /**
  * RadixConverter
  *
  * Assignment 3.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211079 - Dhiyaa Unnisa <unnisadhiyaa25@gmail.com>
  *
  */
 public class RadixConverter {
 
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Masukkan angka");
+        int n = in.nextInt();
+        int m = n;
+        int o = n;
+        int[] twoMultiplier = new int[8];
+        int[] eightMultiplier = new int[8];
+        int[] sixTeenMultiplier = new int[8];
+        int[] base2 = new int[8];
+        int[] base8 = new int[8];
+        int[] base16 = new int[8];
+        int sum;
     }
 }
