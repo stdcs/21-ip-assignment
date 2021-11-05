845a069bfaf85ac6576101841f6b472efcf6cad8
Hafieddz <hasmy41@gmail.com>
2021-11-05 22:31:43 +0800

    feat(arrray/RadixConverter.java): add solution for base 2

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -18,7 +18,32 @@ public class RadixConverter {
         int [] twoMultiplier = new int[8];
         int [] eightMultiplier = new int[8];
         int [] sixteenMultiplier = new int[8];
+        int[] base2 = new int[8];
+        int[] base8 = new int[8];
+        int[] base16 = new int[8];
         int sum; 
 
+        // Base 2 
+        for (int i = 0; i < 8; i++) {
+            sum = 1;
+            for (int j = 0; j < i; j++) {
+                sum = sum * 2;
+            }
+            twoMultiplier[i] = sum;
+        }
+
+        while (n >= 1 ) {
+            for (int i = 7; i >= 0; i++) {
+                while (n >= twoMultiplier[i]) {
+                    n = n - twoMultiplier[i];
+                    base2[7-i]++;
+                }
+            }
+        } 
+        System.out.println("Base 2: ");
+        for (int i = 0; i < twoMultiplier.length; i++) {
+            System.out.println(base2[i]);
+        }
+        System.out.println();
     }
 }
