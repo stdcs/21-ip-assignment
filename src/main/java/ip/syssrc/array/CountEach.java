58976e5f9d1e923d4cc0d8bf69219f82d09061a9
MuhammadDzackyIlhamgit <dzackyilham04@gmail.com>
2021-11-07 00:15:48 +0800

    feat(array/CountEach): add pemrograman

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -5,13 +5,31 @@ package ip.syssrc.array;
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211071 - Muhammad Dzacky Ilham <dzackyilham01@gmail.com>
  *
  */
-
+import java.util.Scanner;
 public class CountEach {
 
     public static void main(String[] args) {
+        Scanner kyy = new Scanner(System.in);
+        int n = kyy.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = kyy.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
 
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        kyy.close();
+       
     }
 }
+
+    
+
