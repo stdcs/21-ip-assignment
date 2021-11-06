7b404427f174ef3d8058ec6098a16a9c0d9e474b
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-11-06 23:03:21 +0800

    fix(array/CountEach): change code

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -14,14 +14,28 @@ public class CountEach {
     public static void main(String[] args) {
         Scanner dede = new Scanner(System.in);
         int n = dede.nextInt();
-        int[] array = new int[10000000];
+        int[] array = new int[n];
+        
         for (int i = 0; i < n; i++) {
-            int var = in.nextInt();
-            array[var]++;
+            array[i] = dede.nextInt();
         }
+
+        int[] freq = new int[array.length];
+        int included = -1;
+
         for (int i = 0; i < array.length; i++) {
-            if (array[i] != 0) {
-                System.out.println(array[i] + " angka " + i);
+            int counter = 1;
+            for (int j = i + 1; j < array.length; j++) {
+               if (array[i] == array[j]) {
+                   counter++;
+                   freq[j] = included;
+               }
+            }
+            if (freq[i] != included) {
+                freq[i] = counter;
+            }
+            if (freq[i] != included) {
+                System.out.println(freq[i] + " angka " + array[i]);
             }
         }
     }
