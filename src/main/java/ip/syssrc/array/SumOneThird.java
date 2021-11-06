61c6bacf68e04a2810771a397c71f9a9ceb5c224
dirganoob <92706538+dirganoob@users.noreply.github.com>
2021-11-06 09:21:11 +0800

    Update SumOneThird.java

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,7 +5,7 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071211080 - Dirgantry Leoonard Nugrah Boro
+ * @author H071211080 - Dirgantry Leonard Nugrah Boro
  *         <dirgantryleonardnugrahboro@gmail.com>
  * 
  */
