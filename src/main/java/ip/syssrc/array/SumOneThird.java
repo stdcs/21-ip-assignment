5ab2a293c909e0a5971aecf95daaadaf4d504193
Djulizah <djul.b74123@gmail.com>
2021-11-08 15:25:52 +0800

    fix(array/SumOneThird): fix array

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -12,35 +12,29 @@ public class SumOneThird {
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-        int sum;
-        int aritmatika;
+        int sum = 0;
+        int sumFinal = 0;
 
         for (int index = 0; index < (arr.length / 3); index++) {
-            sum = 0;
-            for (int j = 0; j < 3; j++ ) {
-                int deret = i + (j * aritmatika);
-                sum = sum + arr[deret];
-                if (j == 2) {
-                    System.out.println(arr[deret] + " = " + sum);;
-                } else {
-                    System.out.print(arr[deret] + " + ");
-                }
-            }
+            System.out.print(arr[index] + " + " + arr[index + (arr.length / 3)] + " + " + arr[index + (arr.length - (arr.length / 3))]);
+
+            sum = arr[index] + arr[index + (arr.length / 3)] + arr[index + (arr.length - (arr.length / 3))];
+            sumFinal = sumFinal + arr[index] + arr[index + (arr.length / 3)] + arr[index + (arr.length - (arr.length / 3))];
+
+            System.out.printf(" = %d%s", sum, "\n");
         }
 
-        if (n%3 > 0) {
-            sum = 0;
-            for (int i = ((n/3) * 3); i < n; i++) {
-                sum = sum + arr[i];
-                if (i == n-1) {
-                    System.out.print(array[i] + " = " + sum);
-                } else {
-                    System.out.print(array[i] + " + ");
-                }
-            }
+
+        //print sumFinal
+        System.out.print("Sum of ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("[" + arr[i] + "]");
         }
 
-        in.close();
+        System.out.print(" = " + sumFinal);
+
+        
 
     }
 }
