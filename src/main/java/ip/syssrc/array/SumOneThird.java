53dca29d6e36884b74e51b1235d8544eccf96e19
SyifaUrRahmi <sifasisi3@gmail.com>
2021-11-08 14:55:25 +0800

    feat(array/SumOneThird) : Memperbaiki Program

--- src/main/java/ip/syssrc/array/SumOneThird.java
+++ src/main/java/ip/syssrc/array/SumOneThird.java
@@ -5,7 +5,7 @@ package ip.syssrc.array;
  *
  * Assignment 3.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211066 - Syifa Ur Rahmi <sifasisi3@gmail.com>
  *
  */
 import java.util.Scanner;
@@ -17,29 +17,9 @@ public class SumOneThird {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
         for (int index = 0; index < (arr.length / 3); index++) {
-            int sum = 0;
-            sum += arr[i];
-        }
-        for(int i = 0; i < arr.length / 3; i++) {
-            for (int j = 0; j < 3; j++ ) {
-                int deret = i + (j * (arr.length / 3));
-                sum = sum + arr[deret];
-                if (j == 2) {
-                    System.out.println(arr[deret] + " = " + sum);;
-                } else {
-                    System.out.print(arr[deret] + " + ");
-                }
-            }
-        }
-        if (arr.length%3 > 0) {
-            sum = 0;
-            for (int i = ((arr.length/3) * 3); i < arr.length; i++) {
-                sum = sum + arr[i];
-                if (i == arr.length-1) {
-                    System.out.print(arr[i] + " = " + sum);
-                } else {
-                    System.out.print(arr[i] + " + ");
-                }
+            int n = (arr.length / 3);
+            for (int j = 0; j < 1; j++ ){
+                System.out.println(arr[i] + " + " + (arr[i] + n) + " + " + (arr[i] + (2*n)) + " = " + (arr[i] + (arr[i] + n)+ (arr[i] + (2*n))));
             }
         }
         in.close();
