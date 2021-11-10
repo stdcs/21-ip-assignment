3ebbdfce276e11dd096a7cccd7ea13674c8c8585
muhammadaryaramdani <mhmdaryaramdani@gmail.com>
2021-11-11 02:22:21 +0900

    feat(function/Vigesimal: add code

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -5,7 +5,7 @@ package ip.syssrc.function;
  *
  * Assignment 4.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211052 - Muhammad Arya Ramdani <mhmdaryaramdani@gmail.com>
  */
 public class Vigesimal {
 
@@ -16,7 +16,14 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        return new char[] {};
+        int length = (n >= 20) ? 2 : 1;
+        char[] arr = new char[length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
+        }
+        return arr;
     }
 
     /**
@@ -25,5 +32,18 @@ public class Vigesimal {
      * @return void
      */
     public static void table() {
+        for (int i = 1; i < 20; i++) { 
+            for (int j = 1; j < 20; j++) {
+                char[] arr = toVigesimal (i * j);
+                System.out.printf("%s", (j != 1) ? "\t" : "");
+                for (char c : arr) {
+                    System.out.printf("%s", c);
+                }
+            }
+            System.out.printf("\n");
+        }
+    }
+    public static void main(String[] args) {
+        table();
     }
 }
