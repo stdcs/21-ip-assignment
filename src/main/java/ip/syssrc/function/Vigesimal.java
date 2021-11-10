c37fee2b10274a206112b524217c121a8824acf7
Mhmmdfhkri <mufik2002@gmail.com>
2021-11-10 13:53:10 -0800

    feat(Tugas4/function): add 'java.util.Scanner' object

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -5,7 +5,7 @@ package ip.syssrc.function;
  *
  * Assignment 4.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211056 - Muhammad Fikri <mufik2002@gmail.com>
  */
 public class Vigesimal {
 
@@ -15,15 +15,32 @@ public class Vigesimal {
      * @param n the radix-10 number to be converted
      * @return array of char that represent the radix-20 format
      */
-    public static char[] toVigesimal(int n) {
-        return new char[] {};
+    public static void main(String[] args) {
+        table();
     }
+    
+    public static char[] toVigecimal(int n) {
+        int length = (n >= 20) ? 2 : 1;
+        char[] arr = new char[length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
+        }
+        return arr;
 
-    /**
-     * Print the vigesimal multiplication table
-     *
-     * @return void
-     */
+    }
     public static void table() {
+        for (int i = 1; i < 20; i++) {
+            for (int j = 1; j < 20; j++) {
+                char[] vigecimal = toVigecimal(i*j);
+                String vig = "";
+                for(char k:vigecimal) {
+                    vig += k;
+                }
+                System.out.printf("%3s",vig);
+            }
+            System.out.println("");
+        }
     }
 }
