ed13b9c7d58ff815724500802dd50079710a3a31
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-11-10 22:37:18 +0700

    feature(function/Vigesimal): Change commentar

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -5,7 +5,7 @@ package ip.syssrc.function;
  *
  * Assignment 4.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211064 - Agung Kartika Ardhiyanda <agungardhiyanda25@gmail.com>
  */
 public class Vigesimal {
 
