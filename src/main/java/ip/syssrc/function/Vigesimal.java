0e73c1951117cd667fb5e137fdab24d20ce59744
sofyanox12 <gaero38@gmail.com>
2021-11-10 21:41:07 +0900

    Update Vigesimal.java

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -5,7 +5,7 @@ package ip.syssrc.function;
  *
  * Assignment 4.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211045 - MUHAMMAD SOFYAND DAUD PUJAS <gaero38@gmail.com>
  */
 public class Vigesimal {
 
@@ -16,7 +16,23 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        return new char[] {};
+        char[] vigesimalChar = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J'};
+        int charIndex = 0;
+        int size = (n < 20) ? 1 : 2;
+        int vigesimalIndex = charIndex;
+        char[] vigesimal = new char[size];
+
+        while (n > 0) { // Forms vigesimal array with converted base-20
+            charIndex = n % 20;
+            vigesimal[vigesimalIndex++] = vigesimalChar[charIndex];
+            n /= 20;
+        }
+
+        char[] converted = new char[size];
+        for (int i = 0, t = vigesimal.length - 1; t >= 0; t--) { // Reverse the array
+            converted[i++] = vigesimal[t];
+        }
+        return converted;
     }
 
     /**
@@ -25,5 +41,15 @@ public class Vigesimal {
      * @return void
      */
     public static void table() {
+        for (int i = 1; i <= 19; i++) {
+            for (int t = 1; t <= 19; t++) {
+                char[] result = toVigesimal(i * t);
+                System.out.printf("%s", (t == 1) ? "" : "\t");
+                for (char x : result) {
+                    System.out.printf("%s", x);
+                }
+            }
+            System.out.println();
+        }
     }
 }
