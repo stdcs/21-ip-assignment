588f572335c36d54f12b8f51fa1e3086d187107a
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-11-10 19:14:20 +0700

    feature(Function/Vigesimal): Delete return value

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -16,13 +16,12 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        return new char[] {};
         int length = (n >= 20) ? 2 : 1;
         char[] arr = new char[length];
-        for (int i = arr.length - 1; i >= 0; i--){
-            int hasilbagi = (n % 20);
-            arr[i] = (hasilbagi > 9) ? (char) (hasilbagi + 55 ) : (char) (modulo + '0');
-            n /=20;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
         }
         return arr;
     }
@@ -33,16 +32,17 @@ public class Vigesimal {
      * @return void
      */
     public static void table() {
-        for (int i = 1; i < 20; i++){
-            for (int j = 1; j < 20; j++){
+        for (int i = 1; i < 20; i++) {
+            for (int j = 1; j < 20; j++) {
                 char[] arr = toVigesimal((i * j));
                 System.out.printf("%s", (j !=1) ? "\t" : "");
 
-                for (char c : arr){
+                for (char c : arr) {
                     System.out.printf("%s", c);
                 }
             }
             System.out.printf("\n");
         }
+    }
     }
 }
