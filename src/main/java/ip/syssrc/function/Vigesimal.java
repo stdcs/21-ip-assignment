d9988b6bcfa721857d56951a8082ac9c058c4e65
erwinarif31 <muherwinarif31@gmail.com>
2021-11-10 09:07:03 +0800

    feat(function/Vigesimal): add logic for `toVigesimal(int n)` method

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -16,7 +16,14 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        return new char[] {};
+        int length = (n >= 20) ? 2 : 1;
+        char[] arr = new char[length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
+        }
+        return arr;
     }
 
     /**
