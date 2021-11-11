ce056ea1b606626979328ca0d3abbc080f9df637
FathurRachmanAlamsyah1041 <sembarangm135@gmail.com>
2021-11-11 14:53:35 +0800

    feat(function): add assignment `BigInt.java` `Vigesimal.java` feat(array): fix conflicts `CountEach.java` `RadixConverter.java` `SumOneThird.java`

--- src/main/java/ip/syssrc/array/CountEach.java
+++ src/main/java/ip/syssrc/array/CountEach.java
@@ -5,13 +5,29 @@ package ip.syssrc.array;
  *
  * Assignment 3.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211041 - Fathur Rachman Alamsyah <sembarangm135@gmail.com>
  *
  */
+import java.util.Scanner;
 
 public class CountEach {
 
     public static void main(String[] args) {
 
+        Scanner t3 = new Scanner(System.in);
+        int n = t3.nextInt();
+        int[] array = new int[10000000];
+        for (int i = 0; i < n; i++) {
+            int var = t3.nextInt();
+            array[var]++;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+
+            } else {
+                System.out.println(array[i] + " angka " + i);
+            }
+        }
+        t3.close();
     }
 }
