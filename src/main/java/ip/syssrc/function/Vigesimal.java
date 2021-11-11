1c9484c64076ff5213270f114dbaa99326c7977a
Eka Hanny <ekahnny@gmail.com>
2021-11-11 10:27:00 +0700

    feat(function): add assignment

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -5,9 +5,12 @@ package ip.syssrc.function;
  *
  * Assignment 4.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211058 - Eka Hanny Oktavia <ekahnny@gmail.com>
  */
 public class Vigesimal {
+	 public static void main(String[] args) {
+        table();
+    }
 
     /**
      * Convert a radix-10 (decimal) number into radix-20 (vigesimal) number
@@ -16,6 +19,13 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
+		int length = (n >= 20) ? 2 : 1;
+        char[] arr = new char[length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
+        }
         return new char[] {};
     }
 
@@ -25,5 +35,16 @@ public class Vigesimal {
      * @return void
      */
     public static void table() {
+		for (int i = 1; i < 20; i++) {
+            for (int j = 1; j < 20; j++) {
+                char[] vigecimal = toVigecimal(i*j);
+                String vig = "";
+                for(char k:vigecimal) {
+                    vig += k;
+					 }
+                System.out.printf("%3s",vig);
+            }
+            System.out.println("");
+        }
     }
 }
