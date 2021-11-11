c19c6563ba43bfdc9d89181d1286b54a3319b788
Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
2021-11-11 17:23:25 +0800

    feat(function/Vigesimal): add code

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -16,14 +16,31 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        return new char[] {};
+        int length = (n >= 20) ? 2 : 1;
+        char[] arr = new char[length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
+        }
+        return arr;
     }
-
     /**
      * Print the vigesimal multiplication table
      *
      * @return void
      */
     public static void table() {
+        for (int i = 1; i < 20; i++) {
+            for (int j = 1; j < 20; j++) {
+                char[] vigecimal = toVigecimal(i*j);
+                String vig = "";
+                for(char k:vigecimal) {
+                    vig += k;
+                }
+                System.out.printf("%3s",vig);
+            }
+            System.out.println("");
+        }
     }
 }
