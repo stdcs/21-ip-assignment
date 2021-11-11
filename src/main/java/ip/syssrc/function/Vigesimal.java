d677eadc6dcee52033d24fb381fce6a609a86a93
Djulizah <djul.b74123@gmail.com>
2021-11-11 19:08:03 +0800

    feat(function/Vigesimal): add method

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -5,7 +5,7 @@ package ip.syssrc.function;
  *
  * Assignment 4.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211073 - Djulizah Bonita L
  */
 public class Vigesimal {
 
@@ -16,7 +16,16 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        return new char[] {};
+        int length = (n >= 20)? 2 : 1;
+        char [] arr = new char[length];
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            int modulo = (n % 20);
+            arr[i] = (modulo > 9)? (char) (modulo + 55) : (char) (modulo + '0');
+            n /= 20;
+        }
+
+        return arr;
     }
 
     /**
@@ -25,5 +34,19 @@ public class Vigesimal {
      * @return void
      */
     public static void table() {
+        for (int i = 1; i < 20; i++) {
+            for (int j = 1; j < 20; j++) {
+                char[] vigesimal = toVigesimal(i*j);
+                string vig = "";
+
+                for (char k : vigesimal){
+                    vig += k;
+                }
+
+                System.out.printf("%3s", vig);
+            }
+
+            System.out.println("");
+        }
     }
 }
