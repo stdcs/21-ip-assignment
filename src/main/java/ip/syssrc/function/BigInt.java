24cb16ae73c83d97b29ca7c0cc5ba8cf51cdc869
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-11 11:30:03 +0800

    feat (function/BigInt) : add BigInt code

--- src/main/java/ip/syssrc/function/BigInt.java
+++ src/main/java/ip/syssrc/function/BigInt.java
@@ -5,11 +5,10 @@ package ip.syssrc.function;
  *
  * Assignment 4.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211061 - St. Alya Ardani Aries <stalyaardaniaries@gmail.com>
  *
  */
 public class BigInt {
-
     /**
      * Adds two arrays as the sum of two numbers, this method is akin to the add
      * method from BigInteger class
@@ -19,7 +18,41 @@ public class BigInt {
      * @return array of integer that represents the sum of 2 numbers
      */
     public static int[] add(int[] n, int[] m) {
-        return new int[] {};
+        int sumLength = (n.length > m.length) ? n.length : m.length;
+        int[] sum = new int[sumLength];
+
+        for (int i = 1; i <= sum.length; i++) {
+            int arrIndex = sumLength - i;
+
+            if (n.length - i >= 0) {
+                sum[arrIndex] += n[n.length - i];
+            }
+
+            if (m.length - i >= 0) {
+                sum[arrIndex] += m[(m.length - i)];
+            }
+
+            if (sum[arrIndex] >= 10 && arrIndex != 0) {
+                sum[arrIndex - 1] += sum[arrIndex] / 10;
+                sum[arrIndex] = sum[arrIndex] % 10;
+            }
+        }
+
+        int[] finale;
+        if (sum[0] > 9){
+            finale = new int[sum.length + 1];
+            finale[0] = 1;
+            for (int i = 0 ;i < finale.length-1; i++) {
+                if (sum[i] > 9) {
+                    sum[i] %= 10;
+                }
+                finale[i+1] = sum[i];
+            }
+        }
+        else {
+            finale = sum;
+        }
+        return finale;
     }
 
     /**
@@ -29,7 +62,23 @@ public class BigInt {
      * @return array of char that represent the formatted number
      */
     public static char[] humanFormat(int[] nums) {
-        return new char[] {};
+        int arrLength = nums.length;
+        arrLength = nums.length + ((nums.length - 1) / 3);
+
+        char[] arr = new char[arrLength];
+
+        for (int i = 1, j = 1 ; i < arr.length+1; i++) {
+            int indexSum = arr.length - i;
+            int indexNums = nums.length - j;
+            if ((i % 4) == 0) {
+                arr[indexSum] = '.';
+            }
+            else {
+                arr[indexSum] = (char) (nums[indexNums]+'0');
+                j++;
+            } 
+        }
+        return arr;
     }
 
     /**
@@ -40,7 +89,22 @@ public class BigInt {
      * @return array of char that represent the formatted number
      */
     public static char[] humanFormat(int[] nums, char sparator) {
-        return new char[] {};
+        int arrLength = nums.length;
+        arrLength = nums.length + ((nums.length - 1) / 3);
+        char[] arr = new char[arrLength];
+
+        for (int i = 1, j = 1; i < arr.length + 1; i++) {
+            int indexArr = arr.length - i;
+            int indexNums = nums.length - j;
+
+            if ((i % 4) == 0) {
+                arr[indexArr] = sparator;
+            } else {
+                arr[indexArr] = (char) (nums[indexNums] + '0');
+                j++;
+            }
+        }
+        return arr;
     }
 
     /**
@@ -50,6 +114,9 @@ public class BigInt {
      * @return void
      */
     public static void print(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.printf("%d%s", nums[i], (i == nums.length - 1) ? "\n" : "");
+        }
     }
 
     /**
@@ -60,5 +127,8 @@ public class BigInt {
      * @return void
      */
     public static void print(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            System.out.printf("%c%s", chars[i], (i == chars.length - 1) ? "\n" : "");
+        }
     }
 }
