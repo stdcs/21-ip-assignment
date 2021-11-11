0d4f45855f06fef795ab489290a7401b9d477f33
erwinarif31 <muherwinarif31@gmail.com>
2021-11-11 16:55:46 +0800

    fix(function/Vigesimal): fix `toVigesimal()` and `table()`
    toVigesimal: fix arr length, so its not limited to only up 2
    table(): using "\t" at the end of loop

--- src/main/java/ip/syssrc/function/Vigesimal.java
+++ src/main/java/ip/syssrc/function/Vigesimal.java
@@ -16,13 +16,24 @@ public class Vigesimal {
      * @return array of char that represent the radix-20 format
      */
     public static char[] toVigesimal(int n) {
-        int length = (n >= 20) ? 2 : 1;
+
+        int length = 0;
+        int decimal = n;
+        char[] vigechars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                'I', 'J' };
+
+        while (n > 0) {
+            n /= 20;
+            length++;
+        }
+
         char[] arr = new char[length];
         for (int i = arr.length - 1; i >= 0; i--) {
-            int modulo = (n % 20);
-            arr[i] = (modulo > 9) ? (char) (modulo + 55) : (char) (modulo + '0');
-            n /= 20;
+            int modulo = (decimal % 20);
+            decimal /= 20;
+            arr[i] = vigechars[modulo];
         }
+
         return arr;
     }
 
@@ -35,12 +46,13 @@ public class Vigesimal {
         for (int i = 1; i < 20; i++) {
             for (int j = 1; j < 20; j++) {
                 char[] arr = toVigesimal((i * j));
-                System.out.printf("%s", (j != 1) ? "\t" : "");
 
                 /** Print Char Array */
                 for (char c : arr) {
-                    System.out.printf("%s", c);
+                    System.out.printf("%c", c);
                 }
+                System.out.printf("\t");
+
             }
             System.out.printf("\n");
         }
