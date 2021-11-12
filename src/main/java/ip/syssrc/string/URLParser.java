1f5677de8e06bd330e126844e3cd339b780c67e0
fitrh <fitrahm17h@student.unhas.ac.id>
2021-11-12 17:38:04 +0800

    feat(string): add assignment 5.1 placeholder file
    Assignment description: 1. URI Percent Parser at #110

---
+++ src/main/java/ip/syssrc/string/URLParser.java
@@ -0,0 +1,32 @@
+package ip.syssrc.string;
+
+/**
+ * URLParser
+ *
+ * Assignment 5.1
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *
+ */
+public class URLParser {
+
+    /**
+     * Encode rawURL into percent-encoding format
+     *
+     * @param rawURL the URL to be encoded
+     * @return string of encoded URL
+     */
+    public static String encode(String rawURL) {
+        return new String();
+    }
+
+    /**
+     * Decode percent-encoding URL into raw URL
+     *
+     * @param encodedURL the percent-encoding URL to be decoded
+     * @return string of decoded URL
+     */
+    public static String decode(String encodedURL) {
+        return new String();
+    }
+}
