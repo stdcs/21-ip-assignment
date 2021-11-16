7a39b8de4d6ea49c6486cc9ec33de8bc3cb3dcb5
sofyanox12 <gaero38@gmail.com>
2021-11-17 00:57:58 +0900

    Update FizzBuzz.java

--- src/main/java/ip/syssrc/string/FizzBuzz.java
+++ src/main/java/ip/syssrc/string/FizzBuzz.java
@@ -5,7 +5,7 @@ package ip.syssrc.string;
  *
  * Assignment 5.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211045 - MUHAMMAD SOFYAN DAUD PUJAS <gaero38@gmail.com>
  */
 public class FizzBuzz {
 
@@ -13,10 +13,33 @@ public class FizzBuzz {
      * Format a word into Fizz for each F or f and Buzz for each B or b
      *
      * @param word the word to be converted into FIzzBuzz
-     * @return string of the FizzBuzz format of the word or the word it self if
-     *         it doesn't contain 'Ff' or 'Bb'
+     * @return string of the FizzBuzz format of the word or the word it self if it
+     *         doesn't contain 'Ff' or 'Bb'
      */
     public static String format(String word) {
-        return new String();
+        int fizzCount = word.length() - word.toLowerCase().replace("f", "").length();
+        int buzzCount = word.length() - word.toLowerCase().replace("b", "").length();
+        String converted = "";
+        Boolean convert = true;
+
+        if (fizzCount <= 0 && buzzCount <= 0) {
+            convert = false;
+            converted = word;
+        }
+
+        for (int i = 0; i < fizzCount; i++) {
+            if (!convert) {
+                break;
+            }
+            converted = converted.concat("Fizz");
+        }
+        for (int i = 0; i < buzzCount; i++) {
+            if (!convert) {
+                break;
+            }
+            converted = converted.concat("Buzz");
+        }
+
+        return converted;
     }
 }
