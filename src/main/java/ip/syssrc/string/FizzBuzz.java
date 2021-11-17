52101f56488cdf6b45399cb7aee0645307602fc7
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-11-18 02:59:30 +0700

    feature(String/FizzBuzz): Initialize variable, add while loop and if statement

--- src/main/java/ip/syssrc/string/FizzBuzz.java
+++ src/main/java/ip/syssrc/string/FizzBuzz.java
@@ -17,6 +17,30 @@ public class FizzBuzz {
      *         it doesn't contain 'Ff' or 'Bb'
      */
     public static String format(String word) {
-        return new String();
+        String fWord;
+        fWord = word.replace("F", "");
+        fWord = word.replace("f", "");
+        int fCount;
+        fCount = word.length() - fWord.length();
+        String bWord;
+        bWord = word.replace("B", "");
+        bWord = word.replace("b", "");
+        int bCount;
+        bCount = word.length() = bWord.length();
+
+        String fizzBuzz = new String();
+
+        int i = 1;
+        if (fCount == 0 && bCount == 0){
+            fizzBuzz = word;
+        }
+        while (i <= fCount){
+            fizzBuzz = fizzBuzz.concat ("Fizz");
+            i++;
+        }
+        while (i <= bCount){
+            fizzBuzz = fizzBuzz.concat ("Buzz");
+        }
+        return new String(fizzBuzz);
     }
 }
