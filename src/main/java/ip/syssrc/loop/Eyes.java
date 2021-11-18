433cd0553a2740bcff2672a09029ff72afdd7b8b
DizaAlysha <dizalysha@gmail.com>
2021-11-19 02:40:20 +0800

    delete

--- src/main/java/ip/syssrc/loop/Eyes.java
+++ src/main/java/ip/syssrc/loop/Eyes.java
@@ -8,18 +8,38 @@ package ip.syssrc.loop;
  * @author H071211086 - Diza Alysha Zahra <dizalysha@gmail.com>
  *
  */
+<<<<<<< HEAD
 import java.util.Scanner;
  public class Eyes {
 
     public static void main(String[] args) {
         Scanner Diza = new Scanner (System.in);
+=======
+public class Eyes {
+
+    public static void main(String[] args) {
+        Scanner Diza = new Scanner(System.in);
+
+>>>>>>> 638cbda (feat(loop/Eyes):add code)
         int n = Diza.nextInt();
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
+<<<<<<< HEAD
                 System.out.printf("%d%s", (i == j) ? 1 : 0, (j <= n) ? " " : "\n");
             }
             System.out.println();
         }
+=======
+                if (i == j) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+        Diza.close();
+>>>>>>> 638cbda (feat(loop/Eyes):add code)
     }
 }
--- src/main/java/ip/syssrc/loop/MatricesPad.java
+++ src/main/java/ip/syssrc/loop/MatricesPad.java
@@ -8,6 +8,7 @@ package ip.syssrc.loop;
  * @author H071211086 - Diza Alysha Zahra <dizalysha@gmail.com>
  *
  */
+<<<<<<< HEAD
 import java.util.Scanner;
 public class MatricesPad {
 
@@ -94,5 +95,101 @@ public class MatricesPad {
                 System.out.println();
             }
         }
+=======
+
+public class MatricesPad {
+
+    public static void main(String[] args) {
+        Scanner Dz = new Scanner(System.in);
+
+        int start = Dz.nextInt();
+        int end = Dz.nextInt();
+        int size = Dz.nextInt();
+
+        int range = end - start;
+        int temp = start;
+
+        if (start <= 9 && end <= 9) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%01d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%01d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%01d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%01d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start <= 99 && end <= 99) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%02d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%02d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%02d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%02d ", temp);
+                    temp--;
+                }
+            }
+        } else if (start <= 999 && end <= 999) {
+            if (range >= 0) {
+                for (int i = 1; i <= range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%03d ", temp);
+                        temp++;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= range % size; k++) {
+                    System.out.printf("%03d ", temp);
+                    temp++;
+                }
+            } else if (range <= 0) {
+                for (int i = 1; i <= -range / size; i++) {
+                    for (int j = 1; j <= size; j++) {
+                        System.out.printf("%03d ", temp);
+                        temp--;
+                    }
+                    System.out.println("");
+                }
+                for (int k = 0; k <= -range % size; k++) {
+                    System.out.printf("%03d ", temp);
+                    temp--;
+                }
+            }
+        }
+
+        Dz.close();
+>>>>>>> 638cbda (feat(loop/Eyes):add code)
     }
 }
