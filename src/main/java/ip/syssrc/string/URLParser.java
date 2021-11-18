70db3a6d4f7101863852613e7f25c49d44344c40
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-11-18 16:19:36 +0700

    feat (string/URLParser) : add for, if (encodedURL)

--- src/main/java/ip/syssrc/string/URLParser.java
+++ src/main/java/ip/syssrc/string/URLParser.java
@@ -40,6 +40,12 @@ public class URLParser {
 
         String[] symbol = {"%","$", "&", "+", ",", "/", ":", ";", "=", "?", "@", " ", "\"", "<", ">", "#",  "{", "}", "\\", "^", "~", "[", "]", "`"}; 
         String[] URL = {"%24", "%26", "%2b", "%2c", "%2f", "%3a", "%3b", "%3d", "%3f", "%40", "%20", "%22", "%3c", "%3e", "%23", "%25", "%7b", "%7d", "%5c", "%5e", "%7e", "%5b", "%5d", "%60"};
+
+        for (int i = 0; i < URL.length; i++) {
+            if (encodedURL.contains(URL[i])) {
+                encodedURL = encodedURL.replaceAll(URL[i],symbol[i]);
+            }
+        }
         
         return new String();
     }
