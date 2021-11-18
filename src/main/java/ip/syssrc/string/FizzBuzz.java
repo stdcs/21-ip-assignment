0cb61f1b170c7bb0d58f1f4bca8bbab5b3e6c1c7
MuhammadDzackyIlhamgit <dzackyilham04@gmail.com>
2021-11-18 19:54:51 +0800

    feat(string/FizzBuzz): add code

--- src/main/java/ip/syssrc/string/FizzBuzz.java
+++ src/main/java/ip/syssrc/string/FizzBuzz.java
@@ -5,18 +5,34 @@ package ip.syssrc.string;
  *
  * Assignment 5.3
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211071 - Muhammad Dzacky Ilham <dzackyilham01@gmail.com>s
  */
 public class FizzBuzz {
 
-    /**
-     * Format a word into Fizz for each F or f and Buzz for each B or b
-     *
-     * @param word the word to be converted into FIzzBuzz
-     * @return string of the FizzBuzz format of the word or the word it self if
-     *         it doesn't contain 'Ff' or 'Bb'
-     */
-    public static String format(String word) {
-        return new String();
+    
+public static String format(String word) {
+    String charFf = word.replaceAll("f", "").replaceAll("F", "");
+        int freqFf = word.length() - charFf.length();
+
+        String charBb = word.replaceAll("b", "").replaceAll("B", "");
+        int freqBb = word.length() - charBb.length();
+
+
+        String fizzBuzz = new String();
+        if (freqBb == 0 && freqFf == 0) {
+            fizzBuzz = word;
+        }
+
+        for (int i = 0; i < freqFf; i++) {
+            fizzBuzz = fizzBuzz.concat("Fizz");
+        }
+
+        for (int i = 0; i < freqBb; i++) {
+            fizzBuzz = fizzBuzz.concat("Buzz");
+        }
+      
+
+        return new String(fizzBuzz);
+       
     }
 }
