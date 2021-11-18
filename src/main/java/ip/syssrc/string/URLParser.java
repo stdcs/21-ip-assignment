9bd93aafe01aa2e4dab4adcd7227982eb630fcca
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-18 17:21:31 +0800

    feat (string/URLParser) : add for and if code

--- src/main/java/ip/syssrc/string/URLParser.java
+++ src/main/java/ip/syssrc/string/URLParser.java
@@ -19,6 +19,12 @@ public class URLParser {
     public static String encode(String rawURL) {
         String[] symbol = {"$", "&", "+", ",", "/", ":", ";", "=", "?", "@", " ", "\"", "<", ">", "#", "%", "{", "}", "\\", "^", "~", "[", "]", "`"}; 
         String[] URL = {"%24", "%26", "%2b", "%2c", "%2f", "%3a", "%3b", "%3d", "%3f", "%40", "%20", "%22", "%3c", "%3e", "%23", "%25", "%7b", "%7d", "%5c", "%5e", "%7e", "%5b", "%5d", "%60"};
+
+        for (int i = 0; i < symbol.length; i++) {
+            if (rawURL.contains(symbol[i])) {
+                rawURL = rawURL.replaceAll(symbol[i], URL[i]);
+            }
+        }
         
         return new String();
     }
