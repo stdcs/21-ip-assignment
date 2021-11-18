3296eef81667be394092aa80b9fb35403be47e02
stalyaardani <stalyaardaniaries@gmail.com>
2021-11-18 16:53:00 +0800

    feat (string/FizzBuzz) : add String fizzBuzz = new String();

--- src/main/java/ip/syssrc/string/FizzBuzz.java
+++ src/main/java/ip/syssrc/string/FizzBuzz.java
@@ -22,7 +22,17 @@ public class FizzBuzz {
 
         String charBb = word.replaceAll("b", "").replaceAll("B", "");
         int freqBb = word.length() - charBb.length();
-        
+
+        String fizzBuzz = new String();
+        if (freqBb == 0 && freqFf == 0) {
+            fizzBuzz = word;
+        }
+        for (int i = 0; i < freqFf; i++) {
+            fizzBuzz = fizzBuzz.concat("Fizz");
+        }
+        for (int i = 0; i < freqBb; i++) {
+            fizzBuzz = fizzBuzz.concat("Buzz");
+        }
         return new String();
     }
 }
