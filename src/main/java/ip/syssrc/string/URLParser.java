c75d43b1c541d5a753b8420d117df201a213a0e0
sofyanox12 <gaero38@gmail.com>
2021-11-18 19:50:42 +0900

    Update URLParser.java
    Remove both first condition of encode and decode methods which doesn't meet the requirement according to array elements.

--- src/main/java/ip/syssrc/string/URLParser.java
+++ src/main/java/ip/syssrc/string/URLParser.java
@@ -22,14 +22,11 @@ public class URLParser {
         String[] encoder = { "%24", "%26", "%2B", "%2C", "%2F", "%3A", "%3B", "%3D", "%3F", "%40", "%20", "%22", "%3C",
                 "%3E", "%23", "%25", "%7B", "%7D", "%5C", "%5E", "%7E", "%5B", "%5D", "%60" };
 
-        if (!rawURL.contains("%")) {
-            for (int i = 0; i < encoder.length; i++) {
-                if (rawURL.contains(reserved[i])) {
-                    rawURL = rawURL.replaceAll(reserved[i], encoder[i]);
-                }
+        for (int i = 0; i < encoder.length; i++) {
+            if (rawURL.contains(reserved[i])) {
+                rawURL = rawURL.replaceAll(reserved[i], encoder[i]);
             }
         }
-
         return new String(rawURL);
     }
 
@@ -45,14 +42,11 @@ public class URLParser {
         String[] decoder = { "%24", "%26", "%2B", "%2C", "%2F", "%3A", "%3B", "%3D", "%3F", "%40", "%20", "%22", "%3C",
                 "%3E", "%23", "%25", "%7B", "%7D", "%5C", "%5E", "%7E", "%5B", "%5D", "%60" };
 
-        if (!encodedURL.contains("\\") || !encodedURL.contains("/")) {
-            for (int i = 0; i < decoder.length; i++) {
-                if (encodedURL.toUpperCase().contains(decoder[i])) {
-                    encodedURL = encodedURL.toUpperCase().replaceAll(decoder[i], unsafe[i]).toLowerCase();
-                }
+        for (int i = 0; i < decoder.length; i++) {
+            if (encodedURL.toUpperCase().contains(decoder[i])) {
+                encodedURL = encodedURL.toUpperCase().replaceAll(decoder[i], unsafe[i]).toLowerCase();
             }
         }
-
         return new String(encodedURL);
     }
 }
