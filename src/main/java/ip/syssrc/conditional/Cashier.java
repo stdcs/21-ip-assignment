fafad97da9dbd41e8ad2a1d463a90a19223c36eb
Liska <rombeliska@gmail.com>
2021-11-18 21:06:29 +0800

    feat (conditional/Cashier): add code

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -109,17 +109,17 @@ public class Cashier {
         int pembayaran = In.nextInt();
 =======
 
+        int uang, totalBelanja;
         try {
-            System.out.println("Berapa total harga belanja anda ? ");
-            int totalBelanja = in.nextInt();
-
-            System.out.println("Berapa total uang anda bayarkan ? ");
-            int pembayaran = in.nextInt();   
-        } catch (InputMismatch Exception e) {
+            System.out.print("Berapakah total harga belanja anda ? ");
+            totalBelanja = in.nextInt();
+            System.out.print("Berapakah total uang anda bayaran ? ");
+            uang = nanda.nextInt();
         } catch (Exception e) {
-            System.out.println("maaf pastikan inputan anda adalah angka");
+            System.out.println("Maaf, pastikan inputan anda adalah angka");
             return;
         }
+<<<<<<< HEAD
        
 <<<<<<< HEAD
 
@@ -176,36 +176,85 @@ public class Cashier {
             int pecahan = (Kembalian / 10000);
             Kembalian -= (10000 * pecahan);
             System.out.println(pecahan + " lembar uang Rp. 100.000");
+=======
+>>>>>>> dc9b96c (fix (conditional/cashier): add 'change code' object)
 
-        }
-        if (kembalian >= 5000) {
-            kembalian = kembalian - 5000;
-            e = e + 1;
-            int pecahan = (Kembalian / 5000);
-            Kembalian -= (5000 * pecahan);
-            System.out.println(pecahan + " lembar uang Rp. 100.000");
 
-        }   
-        if (kembalian >= 2000) {
-            kembalian = kembalian - 2000;
-            f = f + 1;
-        }
-        if (kembalian >= 2000) {
-            kembalian = kembalian - 2000;
-            f = f + 1;
-            int pecahan = (Kembalian / 2000);
-            Kembalian -= (2000 * pecahan);
-            System.out.println(pecahan + " lembar uang Rp. 100.000");
 
-        }
-        if (kembalian >= 1000) {
-            kembalian = kembalian - 1000;
-            g = g + 1;
-            int pecahan = (Kembalian / 1000);
-            Kembalian -= (1000 * pecahan);
-            System.out.println(pecahan + " lembar uang Rp. 100.000");
 
+        int kembalian = uang - totalBelanja;
+        if (uang > totalBelanja) {
+            System.out.println("Total kembalian anda: Rp. " +kembalian);
+            System.out.println("Dengan rincian sebagai berikut:");
+            System.out.println();
+            System.out.println();
+
+
+
+
+            System.out.println();        
+            int pecahan;
+            if (kembalian >= 100000) {
+                pecahan = (kembalian / 100000);
+                kembalian -= (100000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 100.000");
+            } 
+            if (kembalian >= 50000) {
+                pecahan = (kembalian / 50000);
+                kembalian -= (50000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 50.000");
+            } 
+            if (kembalian >= 20000) {
+                pecahan = (kembalian / 20000);
+                kembalian -= (20000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 20.000");
+            } 
+            if (kembalian >= 10000) {
+                pecahan = (kembalian / 10000);
+                kembalian -= (10000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 10.000");
+            } 
+            if (kembalian >= 5000) {
+                pecahan = (kembalian / 5000);
+                kembalian -= (5000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 5.000");
+            } 
+            if (kembalian >= 2000) {
+                pecahan = (kembalian / 2000);
+                kembalian -= (2000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 2.000");
+            } 
+            if (kembalian >= 1000) {
+                pecahan = (kembalian / 1000);
+                kembalian -= (1000 * pecahan);
+                System.out.println(pecahan+ " lembar uang Rp. 1.000");
+            } 
+            if (kembalian >= 500) {
+                pecahan = (kembalian / 500);
+                kembalian -= (500 * pecahan);
+                System.out.println(pecahan+ " buah uang Rp. 500");
+            } 
+            if (kembalian >= 200) {
+                pecahan = (kembalian / 200);
+                kembalian -= 200 * pecahan;
+                System.out.println(pecahan+ " buah uang Rp. 200");
+            } 
+            if (kembalian >= 100) {
+                pecahan = (kembalian / 100);
+                kembalian -= 100 * pecahan;
+                System.out.println(pecahan+ " buah uang Rp. 100");
+            }
+            if (kembalian > 0) {
+                System.out.println("Dan sebanyak Rp. " + kembalian + " didonasikan");
+            }
+        }else if (uang < totalBelanja){
+        } else if (uang < totalBelanja) {
+            System.out.println("Maaf, pembayaran anda belum mencukupi");
+        }else {
+        } else {
+            System.out.println("Terimakasih telah membayar dengan uang pas");
         }
+<<<<<<< HEAD
         if (kembalian >= 500) {
             kembalian = kembalian - 500;
             h = h + 1;
@@ -280,4 +329,7 @@ public class Cashier {
 
         in.close();
 >>>>>>> c955f73 (fix (conditional/Cashier): add 'change code' object)
+=======
+>>>>>>> dc9b96c (fix (conditional/cashier): add 'change code' object)
     }
+}
