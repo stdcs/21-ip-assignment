46627a6a899f7cfaa01d30c46abeef4fa52fa8e3
DewaRescueVirgiawansyah <dr3scue2003@gmail.com>
2021-11-18 18:56:57 +0800

    feat(string): Pengumpulan Tugas 5

---
+++ src/main/java/ip/syssrc/string/Vocab.java
@@ -0,0 +1,56 @@
+package ip.syssrc.string;
+
+/**
+ * Vocab
+ *
+ * Assignment 5.2
+ *
+ * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ *
+ */
+public class Vocab {
+
+    /**
+     * Add 'un' prefixes to a word, e.g. happy -> unhappy
+     *
+     * @param word the word to be prefixed
+     * @return string of prefixes word
+     */
+    public static String addPrefixUn(String word) {
+        return new String();
+    }
+
+    /**
+     * Add prefix for each word of groups of vocabulary words
+     *
+     * @param prefix the prefix to be added to each word
+     * @param groups the groups of vocabulary words to be prefixed
+     * @return string in the form of:
+     *         `prefix :: prefixword_1 :: prefixword_2 :: ... :: prefixword_n`
+     */
+    public static String prefixes(String prefix, String... groups) {
+        return new String();
+    }
+
+    /**
+     * Remove 'ness' suffix from a word
+     *
+     * @param word the words with suffix to be removed
+     * @return string of word without 'ness' suffix
+     */
+    public static String removeSuffixNess(String word) {
+        return new String();
+    }
+
+    /**
+     * Extract an adjective from a sentence and turning it into a verb
+     *
+     * @param sentence the sentence to be used as vocabulary word
+     * @param index    the position of adjective word inside the sentence, -1 means
+     *                 last position
+     * @return string of extracted adjective as a verb
+     */
+    public static String nounToVerb(String sentence, int index) {
+        return new String();
+    }
+}
