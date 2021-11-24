98eecb297dcb9e715eee69c53c0473ccc4bc5b8a
febift <febifian23@gmail.com>
2021-11-24 13:11:33 +0700

    feat(conditional/RoleAccess):fix code

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -12,6 +12,7 @@ public class RoleAccess {
 
     public static void main(String[] args) {
         int menuIndex = 0;
+<<<<<<< HEAD
         int subMenuIndex = 0;
         boolean isValidRole = false;
 
@@ -39,6 +40,31 @@ public class RoleAccess {
               System.out.printf("%2d.%d. CRUD User \n", menuIndex, subMenuIndex);
                System.out.println();
             case "Admin":
+=======
+    int subMenuIndex = 0;
+    boolean isValidRole = false;
+
+    if (args.length == 0){
+      System.out.println("What role you want to see ?");
+      System.out.println("For example, try 'Admin'.");
+    }else if(args.length == 1){
+      if (args[0].equals("Super Admin") || args[0].equals("Admin") || args[0].equals("User")) {
+        isValidRole = true;
+      }
+      if(isValidRole == true){
+          switch (args[0] ){
+              case "Super Admin":
+                menuIndex = menuIndex + 1;
+                System.out.printf("%d. Super Admin\n", menuIndex);
+                subMenuIndex = subMenuIndex + 1;
+                System.out.printf("%2d.%d. CRUD Super Admin\n", menuIndex, subMenuIndex);
+                subMenuIndex = subMenuIndex + 1;
+                System.out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex);
+                subMenuIndex = subMenuIndex + 1;
+                System.out.printf("%2d.%d. CRUD User \n", menuIndex, subMenuIndex);
+                System.out.println();
+              case "Admin":
+>>>>>>> 9a107a0 (feat(conditional/RoleAccess): add 'java.util.Scanner' object)
                 menuIndex = menuIndex + 1;
                 subMenuIndex = 0;
                 System.out.printf("%d. Admin\n", menuIndex);
