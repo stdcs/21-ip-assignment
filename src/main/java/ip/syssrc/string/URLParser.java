1078a0f559b72462a944a5ff0054109281fd39d2
Asehpryanto Rari Parinding <Asehpryantorari@gmail.com>
2021-11-24 23:50:38 +0800

    feat(string/URLParser): add code

--- src/main/java/ip/syssrc/string/URLParser.java
+++ src/main/java/ip/syssrc/string/URLParser.java
@@ -20,7 +20,7 @@ public class URLParser {
         String[] symbol = {"$", "&", "+", ",", "/", ":", ";", "=", "?", "@", " ", "\"", "<", ">", "#", "%", "{", "}", "\\", "^", "~", "[", "]", "`"}; 
         String[] URL = {"%24", "%26", "%2b", "%2c", "%2f", "%3a", "%3b", "%3d", "%3f", "%40", "%20", "%22", "%3c", "%3e", "%23", "%25", "%7b", "%7d", "%5c", "%5e", "%7e", "%5b", "%5d", "%60"};
         
-        for (int i = 0; i < symbol.length; i++) {
+        for (int i = 0; i < symbol.length; i++) { 
             if (rawURL.contains(symbol[i])) {
                 rawURL = rawURL.replaceAll(symbol[i], URL[i]);
             }
