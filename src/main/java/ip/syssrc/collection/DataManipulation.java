d61c125dcfa2affa23326c3d6fcaa6058ed9fcff
erwinarif31 <muherwinarif31@gmail.com>
2021-11-29 20:59:09 +0800

    fix(collection/DataManipulation): fix author information

--- src/main/java/ip/syssrc/collection/DataManipulation.java
+++ src/main/java/ip/syssrc/collection/DataManipulation.java
@@ -10,7 +10,7 @@ import java.util.Map;
  *
  * Assignment 6.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211059 - Muhammad Erwin Arif <muherwinarif31@gmail.com>
  *
  */
 public class DataManipulation {
