d70e3c12b4d7a89df9dd2cd84c087030226d6609
MichaelGabriel01 <arknet31@gmail.com>
2021-11-29 23:57:41 +0800

    feat(collection/TemplateParse)

--- src/main/java/ip/syssrc/collection/TemplateParse.java
+++ src/main/java/ip/syssrc/collection/TemplateParse.java
@@ -1,11 +1,16 @@
 package ip.syssrc.collection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 /**
  * TemplateParse
  *
  * Assignment 6.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211068 - Michael Gabriel Bida <michaelgabriel663@gmail.com>
  *
  */
 public class TemplateParse {
@@ -38,7 +43,23 @@ public class TemplateParse {
      * @return list of strings with parsed placeholder
      */
     public static List<String> parse(String template, Map<String, String> data) {
-        return new ArrayList<>();
+        String[] str = template.split("\n");
+        List<String> stringList = new ArrayList<>(Arrays.asList(str));
+
+        /** Perulangan untuk tiap baris */
+        for (int index = 0; index < stringList.size(); index++) {
+            /** Perulangan untuk tiap key */
+            for (String key : data.keySet()) {
+                if (stringList.get(index).contains(key)) {
+                    String placeholder = String.format("\\{%s\\}", key);
+                    stringList.set(index,
+                            stringList.get(index).replaceAll(placeholder, data.get(key)));
+                }
+
+            }
+        }
+
+        return new ArrayList<>(stringList);
     }
 
     /**
@@ -47,4 +68,8 @@ public class TemplateParse {
      * @param template the list to be printed
      **/
     public static void render(List<String> template) {}
+        for (int i = 0; i < template.size(); i++) {
+            System.out.println(template.get(i));
+        }
+    }
 }
