24dbe08cf48314d249411f939bddc7f3bba0e0d4
erwinarif31 <muherwinarif31@gmail.com>
2021-11-29 17:56:08 +0800

    feat(collection/TemplateParse): add objects
    - java.util.ArrayList;
    - java.util.HashMap;
    - java.util.List;
    - java.util.Map;
    - java.util.Arrays;

--- src/main/java/ip/syssrc/collection/TemplateParse.java
+++ src/main/java/ip/syssrc/collection/TemplateParse.java
@@ -1,5 +1,11 @@
 package ip.syssrc.collection;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
+
 /**
  * TemplateParse
  *
