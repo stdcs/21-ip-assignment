cebc6f22774e1f2af1ab87bbd605743294431779
DewaRescueVirgiawansyah <dr3scue2003@gmail.com>
2021-11-30 08:15:07 +0800

    feat(collection): Assignment 6 - 1072

---
+++ src/main/java/ip/syssrc/collection/DataManipulation.java
@@ -0,0 +1,42 @@
+package syssrc.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * DataManipulation
+ *
+ * Assignment 6.2
+ *
+ * @author H071211072 - Dewa Rescue Virgiawansyah <dr3scue2003@gmail.com>
+ *
+ */
+public class DataManipulation {
+
+    public static void main(String[] args) {
+        // List<Map<String, String>> student = new ArrayList<>();
+
+       // student.add(insert("id, first_name, last_name, gender", "H071171512, Fitrah, Muhammad, Male"));
+        // student.add(insert("id, first_name, last_name, gender", "H071171510, Amalia, Dwi, Female"));
+        // student.add(insert("id,first_name,last_name,gender", "H071171504, Rabia,Adaw,Female"));
+        // student.add(insert("id,first_name, last_name, gender", "H071171308,Babang,Arizk,Male"));
+
+        // select(student, "first_name", "last_name").forEach(System.out::println);
+
+        // prettify(select(student, "id", "first_name", "last_name", "gender"));
+
+    }
+
+    public static Map<String, String> insert(/* signatures */) {
+        return new HashMap<>();
+    }
+
+    public static List<String> select(/* signatures */) {
+        return new ArrayList<>();
+    }
+
+    public static void prettify(List<String> strings) {}
+
+}
