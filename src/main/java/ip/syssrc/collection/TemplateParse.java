23ac6dd2de9110c2ab103d3bbffc700b118d895d
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-11-30 19:02:51 +0700

    feature(collection/TemplateParse): Add while loop and if else statement

--- src/main/java/ip/syssrc/collection/TemplateParse.java
+++ src/main/java/ip/syssrc/collection/TemplateParse.java
@@ -1,4 +1,5 @@
 package ip.syssrc.collection;
+
 import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
@@ -9,7 +10,7 @@ import java.util.Arrays;
  *
  * Assignment 6.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211064 - Agung Kartika Ardhiyanda <agungardhiyanda25@gmail.com>
  *
  */
 public class TemplateParse {
@@ -43,14 +44,16 @@ public class TemplateParse {
      */
     public static List<String> parse(String template, Map<String, String> data) {
         String[] str = template.split("\n");
-        List<String> stringlist = new ArrayList<>(Arrays.asList(str));
+        List<String> stringList = new ArrayList<>(Arrays.asList(str));
         
-        for(int i = 0; i < stringList.size(); i++){
-            for(String key : data.keySet()){
-                if (stringList.get(i).contains(key)){
+         
+         for (int i = 0; i < stringList.size(); i++) {
+            
+            for (String key : data.keySet()) {
+                if (stringList.get(i).contains(key)) {
                     String placeholder = String.format("\\{%s\\}", key);
                     stringList.set(i,
-                                    stringList.get(i).replaceAll(placeholder, data.get(key)));
+                            stringList.get(i).replaceAll(placeholder, data.get(key)));
                 }
             }
         }
@@ -62,5 +65,11 @@ public class TemplateParse {
      *
      * @param template the list to be printed
      **/
-    public static void render(List<String> template) {}
+    public static void render(List<String> template) {
+        int i = 0;
+        while (i < template.size()){
+            System.out.println(template.get(i));
+            i++;
+        }
+    }
 }
