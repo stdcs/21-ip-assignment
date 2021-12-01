9de7cfd2f0f02c3fd84a5be2a2e867fa4a0a11b6
Suhastina <Suhastina88@gmail.com>
2021-12-01 22:31:07 +0700

    feat(collection/DataManipulation): add code

--- src/main/java/ip/syssrc/collection/DataManipulation.java
+++ src/main/java/ip/syssrc/collection/DataManipulation.java
@@ -5,7 +5,7 @@ package ip.syssrc.collection;
  *
  * Assignment 6.2
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211084 - Suhastina <Suhastina88@gmaail.com>
  *
  */
 public class DataManipulation {
@@ -25,13 +25,78 @@ public class DataManipulation {
     }
 
     public static Map<String, String> insert(/* signatures */) {
-        return new HashMap<>();
+        String[] keysArr = keys.replaceAll("","").toLowerCase().split(",");
+        String[] argsArr = args.replaceAll("","").split(",");
+
+        if (keysArr.length != argsArr.length){
+            throw new IllegalArgumentException("invalid argument");
+        }
+        Map<String, String> student = new HashMap<>();
+        for (int i = 0; i < argsArr.length; i++) {
+            student.put(keysArr[i], argsArr[i]);
+        }
+        return new HashMap<>(student);
     }
 
-    public static List<String> select(/* signatures */) {
-        return new ArrayList<>();
+    public static List<String> select(List<Map<String, String>> List, String... args) {
+        List<String> selectedMap = new ArrayList<>();
+        String cols = new String("");
+        for (int i = 0; i < args.length; i++) {
+            String separator = (i != 0) ? ";" : "";
+            cols = cols.concat(String.format("%s%s", separator, values));
+        }
+        selectedMap.add(cols);
+        for (int i = 0; i < list.size(); i++) {
+            String rows = new String("");
+            for (int j = 0; j < args.length; j++) {
+                String values = list.get(i).get(args[j]);
+                if (!list.get(i).containskey(args[j])){
+                    values = "None";
+                }
+                String separator = (j != 0) ? ";" : "";
+                rows = rows.concat(String.format("%s%s", separator, values));
+            }
+            selectedMap.add(rows);
+        }
+        return new ArrayList<>(selectedMap);
     }
 
-    public static void prettify(List<String> strings) {}
+    public static void prettify(List<String> strings) {
+        int columnLenghth = 0;
+        for (int i = 0; i < string.size(); i++) {
+            String[] rowNth = strings.get(i).split(";");
+            for (int j = 0; j < rowNth.length; j++) {
+                if (columnLenghth < rowNth[j].length()) {
+                    columnLenghth = rowNth[j].length();
+                }
+            } 
+        }
+        String formatter = "| %" + (-columnLenghth) + "s";
+        String separator = "-";
+
+        string.set(0, strings.get(0).toUpperCase());
+        String[] columnName = strings.get(0).split(";");
+        String firstRows = "";
+
+        for (int i = 0; i < columnName.length; i++) {
+            firstRows = firstRows.concat(String.format(formatter, columnName[i]));
+        }
+        int rowLength = firstRows.length() -1;
+        System.out.printf("%s", firstRows);
+        System.out.println("|");
+
+        System.out.printf("%s\n", separator.repeat(rowLength));
+
+        for (int i = 1; i < strings.size(); i++){
+            String[] rowNth = strings.get(i).split(";");
+            String rows = "";
+            for (int j = 0; j < rowNth.length; j++) {
+                rows = rows.concat(String.format(formatter, rowNth[j]));
+            }
+            System.out.printf("%s", rows);
+            System.out.println("|");
+        }
+        System.out.printf("%s\n", separator.repeat(rowLength)); 
+    }
 
 }
