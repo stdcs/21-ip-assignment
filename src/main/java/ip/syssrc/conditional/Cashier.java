dfd3568202789ce32448a00ad313400da4ceb470
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-12-01 21:57:35 +0800

    delete

--- src/main/java/ip/syssrc/conditional/Cashier.java
+++ src/main/java/ip/syssrc/conditional/Cashier.java
@@ -19,6 +19,7 @@ public class Cashier {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 6ee24d0 (fix(conditional/Cashier): change String to int)
 =======
@@ -371,7 +372,109 @@ public class Cashier {
         } else {
             System.out.println("Terimakasih telah membayar dengan uang pas");
 >>>>>>> 9c99f23 (fix(conditional/Cashier): change code and 'if' block)
+<<<<<<< HEAD
 >>>>>>> d0c2fc9 (delete)
+=======
+=======
+        
+        System.out.print("Berapakah total harga belanja anda ? ");
+        String totalBelanja = nanda.next();
+        System.out.print("Berapakah total uang anda bayaran ? ");
+        String uang = nanda.next();
+        if (totalBelanja.equals(uang)) {
+        }else {
+            System.out.println("Maaf, pastikan inputan anda adalah angka");
+        }
+        
+        
+        
+        int kembalian = Integer.parseInt(uang) - Integer.parseInt(totalBelanja);
+        if (kembalian == 0) {
+            System.out.println("Terimakasih telah membayar dengan uang pas");
+        }else if (kembalian < 0){
+            System.out.println("Maaf, pembayaran anda belum mencukupi");
+        }else {
+            System.out.println("Total kembalian anda: Rp. " +kembalian);
+            System.out.println("Dengan rincian sebagai berikut:");
+            System.out.println();
+            System.out.println();
+
+            int a,b,c,d,e,f,g,h,i,j;
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            e = 0;
+            f = 0;
+            g = 0;
+            h = 0;
+            i = 0;
+            j = 0;
+
+            if (kembalian >= 100000) {
+                kembalian = kembalian - 100000;
+                a = a + 1;
+            } if (kembalian >= 50000) {
+                kembalian= kembalian - 50000;
+                b = b + 1;
+            } if (kembalian >= 20000) {
+                kembalian = kembalian - 20000;
+                c = c + 1;
+            } if (kembalian >= 20000) {
+                kembalian = kembalian - 20000;
+                c = c + 1;
+            } if (kembalian >= 10000) {
+                kembalian = kembalian - 10000;
+                d = d + 1;
+            } if (kembalian >= 5000) {
+                kembalian= kembalian - 5000;
+                e = e + 1;
+            } if (kembalian >= 2000) {
+                kembalian = kembalian - 2000;
+                f = f + 1;
+            } if (kembalian >= 2000) {
+                kembalian = kembalian - 2000;
+                f = f + 1;
+            } if (kembalian >= 1000) {
+                kembalian = kembalian - 1000;
+                g = g + 1;
+            } if (kembalian >= 500) {
+                kembalian = kembalian - 500;
+                h = h + 1;
+            } if (kembalian >= 200) {
+                kembalian = kembalian - 200;
+                i = i + 1;
+            } if (kembalian >= 200) {
+                kembalian = kembalian - 200;
+                i = i + 1;
+            } if (kembalian >= 100) {
+                kembalian = kembalian - 100;
+                j = j + 1;
+            }if (a > 0) {
+                System.out.println(a+ " lembar uang Rp. 100.000");
+            }if (b > 0) {
+                System.out.println(b+ " lembar uang Rp. 50.000");
+            }if (c > 0) {
+                System.out.println(c+ " lembar uang Rp. 20.000");
+            }if (d > 0) {
+                System.out.println(d+ " lembar uang Rp. 10.000");
+            }if (e > 0) {
+                System.out.println(e+ " lembar uang Rp. 5.000");
+            }if (f > 0) {
+                System.out.println(f+ " lembar uang Rp. 2.000");
+            }if (g > 0) {
+                System.out.println(g+ " lembar uang Rp. 1.000");
+            }if (h > 0) {
+                System.out.println(h+ " buah uang Rp. 500");
+            }if (i > 0) {
+                System.out.println(i+ " buah uang Rp. 200");
+            }if (j > 0) {
+                System.out.println(j+ " buah uang Rp. 100");
+            }if (kembalian > 0) {
+                System.out.println("Dan sebanyak Rp. " +kembalian+ " didonasikan");
+            }
+>>>>>>> bf8cc79 (feat(conditional/Cashier) : add 'switch' object)
+>>>>>>> 700d9da (delete)
         }
     }
 }
--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -21,6 +21,7 @@ public class RoleAccess {
 <<<<<<< HEAD
 =======
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         if (args.length == 0) {
             System.out.println("What role you want to see ?");
@@ -258,7 +259,60 @@ public class RoleAccess {
 =======
             System.out.println("Too many argument");
 >>>>>>> de0bd4a (fix(conditional/RoleAccess): change indentasi)
+<<<<<<< HEAD
 >>>>>>> 2d94d8f (delete)
+=======
+=======
+    
+        if (args.length == 0) {
+          System.out.println("What role you want to see ?");
+          System.out.println("For example, try 'Admin'.");
+        
+        } else if (args.length == 1) {
+          if (args[0].equals("Super Admin") || args[0].equals("Admin") || args[0].equals("User")){
+              isValidRole = true;
+          } if (isValidRole == true) {
+            switch (args[0]) {
+                case "Super Admin":
+                    menuIndex = menuIndex + 1;
+                    System.out.printf("%d. Super Admin\n", menuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Super Admin\n", menuIndex , subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Admin\n", menuIndex , subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Userr\n", menuIndex , subMenuIndex);
+                    System.out.println();
+                    
+                case "Admin":
+                    menuIndex = menuIndex + 1;
+                    subMenuIndex = 0;
+                    System.out.printf("%d. Admin\n", menuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD User\n", menuIndex, subMenuIndex);
+                    System.out.println();
+            
+                default:
+                    menuIndex = menuIndex + 1;
+                    subMenuIndex = 0;
+                    System.out.printf("%d. User\n", menuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. View\n", menuIndex, subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. Edit\n", menuIndex, subMenuIndex);
+            
+                    break;
+            }
+          } else {
+            System.out.println("Invalid Role");
+            System.out.println("Valid Role : Super Admin, Admin, User");
+          }
+        } else {
+          System.out.println("Too many argument");
+>>>>>>> bf8cc79 (feat(conditional/Cashier) : add 'switch' object)
+>>>>>>> 700d9da (delete)
         }
     }
 }
