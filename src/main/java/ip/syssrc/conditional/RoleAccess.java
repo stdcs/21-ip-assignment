01fe4cbc3c0aa4d1a6b7f56968f862f5ebba8101
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-12-01 21:52:12 +0800

    delete

--- src/main/java/ip/syssrc/conditional/RoleAccess.java
+++ src/main/java/ip/syssrc/conditional/RoleAccess.java
@@ -14,6 +14,12 @@ public class RoleAccess {
         int menuIndex = 0;
         int subMenuIndex = 0;
         boolean isValidRole = false;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 5eb0d90 (delete)
 
         if (args.length == 0) {
             System.out.println("What role you want to see ?");
@@ -68,6 +74,70 @@ public class RoleAccess {
             } else {
                 System.out.println("Too many argument");
             }
+<<<<<<< HEAD
+=======
+        } else {
+<<<<<<< HEAD
+<<<<<<< HEAD
+          System.out.println("Too many argument");
+>>>>>>> bf8cc79 (feat(conditional/Cashier) : add 'switch' object)
+=======
+              System.out.println("Too many argument");
+>>>>>>> b42662b (fix (conditional/RoleAcces): change indentasi to '4 space')
+=======
+            System.out.println("Too many argument");
+>>>>>>> 391b6fd (fix(conditional/RoleAccess): change indentasi)
+=======
+    
+        if (args.length == 0) {
+          System.out.println("What role you want to see ?");
+          System.out.println("For example, try 'Admin'.");
+        
+        } else if (args.length == 1) {
+          if (args[0].equals("Super Admin") || args[0].equals("Admin") || args[0].equals("User")){
+              isValidRole = true;
+          } if (isValidRole == true) {
+            switch (args[0]) {
+                case "Super Admin":
+                    menuIndex = menuIndex + 1;
+                    System.out.printf("%d. Super Admin\n", menuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Super Admin\n", menuIndex , subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Admin\n", menuIndex , subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Userr\n", menuIndex , subMenuIndex);
+                    System.out.println();
+                    
+                case "Admin":
+                    menuIndex = menuIndex + 1;
+                    subMenuIndex = 0;
+                    System.out.printf("%d. Admin\n", menuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD Admin\n", menuIndex, subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. CRUD User\n", menuIndex, subMenuIndex);
+                    System.out.println();
+            
+                default:
+                    menuIndex = menuIndex + 1;
+                    subMenuIndex = 0;
+                    System.out.printf("%d. User\n", menuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. View\n", menuIndex, subMenuIndex);
+                    subMenuIndex = subMenuIndex + 1;
+                    System.out.printf("%2d.%d. Edit\n", menuIndex, subMenuIndex);
+            
+                    break;
+            }
+          } else {
+            System.out.println("Invalid Role");
+            System.out.println("Valid Role : Super Admin, Admin, User");
+          }
+        } else {
+          System.out.println("Too many argument");
+>>>>>>> d942e9b (feat(conditional/Cashier) : add 'switch' object)
+>>>>>>> 5eb0d90 (delete)
         }
     }
 }
