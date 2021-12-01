928349a2bba97620523e36ce48c6f888947ec70c
Wd.AnandaLesmono <nandalesmono@gmail.com>
2021-12-01 22:09:30 +0800

    feat(collection/TemplateParse): add code

--- src/main/java/ip/syssrc/collection/TemplateParse.java
+++ src/main/java/ip/syssrc/collection/TemplateParse.java
@@ -8,6 +8,11 @@ package ip.syssrc.collection;
  * @author H071211074 - Wd. Ananda Lesmono <nandalesmono@gmail.com>
  *
  */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class TemplateParse {
 
     public static void main(String[] args) {
@@ -38,7 +43,20 @@ public class TemplateParse {
      * @return list of strings with parsed placeholder
      */
     public static List<String> parse(String template, Map<String, String> data) {
-        return new ArrayList<>();
+        String[] args = template.split("\n");
+        List<String> list = new ArrayList<>(Arrays.asList(args));
+
+        for (int i = 0; i < list.size(); i++) {
+            for (String key : data.keySet()) {
+                    String placeholder = String.format("\\{%s\\}", key);
+                    list.set(i, list.get(i).replaceAll(placeholder, data.get(key)));
+            }
+        }
+        for (String key : data.keySet()) { 
+            String placeholder = String.format("\\{%s\\}", key);
+            template = (template.replaceAll(placeholder, data.get(key)));
+        }
+        return list;
     }
 
     /**
@@ -46,5 +64,7 @@ public class TemplateParse {
      *
      * @param template the list to be printed
      **/
-    public static void render(List<String> template) {}
+    public static void render(List<String> template) {
+        template.forEach(System.out :: println);
+    }
 }
