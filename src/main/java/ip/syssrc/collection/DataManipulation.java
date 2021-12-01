02dd1b9c6f82641f28ae5b997718b6d050b273f5
AgungArdhiyanda <agungardhiyanda25@gmail.com>
2021-12-01 12:57:40 +0700

    feature(collection/DataManipulation): Fix while loop and delete commentar

--- src/main/java/ip/syssrc/collection/DataManipulation.java
+++ src/main/java/ip/syssrc/collection/DataManipulation.java
@@ -15,19 +15,16 @@ import java.util.Map;
 public class DataManipulation {
 
     public static void main(String[] args) {
-        List<Map<String, String>> student = new ArrayList<>();
         
-        student.add(insert("id, first_name, last name, gender", "H071171512, Fitrah, Muhammad, Male"));
-        student.add(insert("id, first_name, last name, gender", "H071171510, Amalia, Dwi, Female"));
-        student.add(insert("id, first_name, last name, gender", "H071171512, Rabia, Adaw, Male"));
-        student.add(insert("id, first_name, last name, gender", "H071171308, Babang, Arizk, Male"));
+        //List<Map<String, String>> student = new ArrayList<>();
+        
+      
         // student.add(insert("id, first_name, last_name, gender", "H071171512, Fitrah, Muhammad, Male"));
         // student.add(insert("id, first_name, last_name, gender", "H071171510, Amalia, Dwi, Female"));
         // student.add(insert("id,first_name,last_name,gender", "H071171504, Rabia,Adaw,Female"));
         // student.add(insert("id,first_name, last_name, gender", "H071171308,Babang,Arizk,Male"));
-        select(student, "first_name", "last_name").forEach(System.out::println);
-        System.out.println();
-        prettify(select(student, "id", "first_name", "last_name", "gender"));
+        
+        
         // select(student, "first_name", "last_name").forEach(System.out::println);
 
         // prettify(select(student, "id", "first_name", "last_name", "gender"));
@@ -52,7 +49,7 @@ public class DataManipulation {
             studentInformation.put(keysArr[i], valuesArr[i]);
             i++;
         }
-                return new HashMap<>(studentInformation);
+                return studentInformation;
     }
 
     public static List<String> select(List<Map<String, String>> list, String... keys) {
@@ -66,6 +63,7 @@ public class DataManipulation {
             i++;
         }
         selectedMap.add(columns);
+
         while(i < list.size()){
             String rows = new String("");
             while (j < keys.length) {
@@ -79,45 +77,59 @@ public class DataManipulation {
             selectedMap.add(rows);
             i++;
         }
-        return new ArrayList<>(selectedMap);
+        return selectedMap;
     }
 
     public static void prettify(List<String> strings) {
-        int columnLength= 0;
-        int i = 0;
-        int j = 0;
-        while ( i < strings.size()){
-            String[] rowNth = strings.get(i).split(";");
-            while (j < rowNth.length ){
-                if( columnLength < rowNth[j].length()) {
-                    columnLength = rowNth[j].length();
-                }
-                j++;
-            }
-            i++;
+       int columnLength = 0;
+       int i = 0;
+       int j = 0;
+       while (i < strings.size()){
+           String[] rowNth = strings.get(i).split(";");
+           while (j < rowNth.length){
+               if(columnLength < rowNth[j].length()){
+                   columnLength = rowNth[j].length();
+               }
+               j++;
+           }
+           i++;
         }
-        
-        while ( i < strings.size()) {
-            String[] rowNth = strings.get(i).split(";");
+
+        String formatter = " | %" + (-columnLength) + "s ";
+        String separator = "-";
+
+        strings.set(0, strings.get(0).toUpperCase());
+        String[] columnName = strings.get(0).split(";");
+        String firstRows = "";
+
+        int a = 0;
+        while (a < columnName.length) {
+            firstRows = firstRows.concat(String.format(formatter, columnName[a]));
+        }
+
+        int rowLength = firstRows.length() - 1;
+        System.out.printf("%s", firstRows);
+
+        System.out.printf(" %s\n", separator.repeat(rowLength));
+
+        int k = 1;
+        int l = 0;
+        while ( k < strings.size()){
+            String[] rowNth = strings.get(k).split(";");
             String rows = "";
-            while ( j < rowNth.length){
-                rows = rows.concat(String.format("%s %" + (-columnLength) + "s |",
-                j == 0 ? "|" : "",
-                i == 0 ? rowNth[j].toUpperCase() : rowNth[j]));
-                j++;
+            while ( l < rowNth.length){
+                rows = rows.concat(String.format(formatter, rowNth[l]));
+                l++;
             }
-           
-
-            System.out.printf("%s\n", rows);
-
-            int rowLength = rows.length();
-            if ( i == 0 || i == strings.size() - 1){
-                String separator = "-";
-                System.out.printf(" %s\n", separator.repeat((rowLength - 2)));
-            }
-            i++;
+            k++;
         }
+        System.out.printf("%s", rows);
+        System.out.println("|");
     }
+
+    System.out.printf(" %s\n", separator.repeat(rowLength));
+    }
+
 }
 
 
