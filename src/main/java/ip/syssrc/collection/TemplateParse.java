8e775b51fe31d9f7b2086dcfaee84c804d9f026a
DhiyaaUnnisa <unnisadhiyaa25@gmail.com>
2021-12-01 18:30:43 +0700

    feat (collection/TemplateParse) : add import

--- src/main/java/ip/syssrc/collection/TemplateParse.java
+++ src/main/java/ip/syssrc/collection/TemplateParse.java
@@ -1,11 +1,16 @@
 package ip.syssrc.collection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 /**
  * TemplateParse
  *
  * Assignment 6.1
  *
- * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
+ * @author H071211079 - Dhiyaa Unnisa <unnisadhiyaa25@gmail.com>
  *
  */
 public class TemplateParse {
