0886a7336481fa06dc8be74860009c4f27126456
stalyaardani <stalyaardaniaries@gmail.com>
2021-12-01 19:42:43 +0800

    feat (collection/Template) : add for code

--- src/main/java/ip/syssrc/collection/TemplateParse.java
+++ src/main/java/ip/syssrc/collection/TemplateParse.java
@@ -64,5 +64,9 @@ public class TemplateParse {
      *
      * @param template the list to be printed
      **/
-    public static void render(List<String> template) {}
+    public static void render(List<String> template) {
+        for (int i = 0; i < template.size(); i++) {
+            System.out.println(template.get(i));
+        }
+    }
 }
