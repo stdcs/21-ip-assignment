fa49943f5991e24a660331a22e9b5375633335d5
febift <febifian23@gmail.com>
2021-12-02 08:15:32 +0700

    fix(array/SumOneThird): fix code

--- src/main/java/ip/syssrc/array/RadixConverter.java
+++ src/main/java/ip/syssrc/array/RadixConverter.java
@@ -11,13 +11,19 @@ import java.util.Scanner;
  *
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 08a8b6c (feat(array):add Assignment3)
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
 =======
  * @author H071211051 - Febi Fiantika  <febifian23@gmail.com>
 >>>>>>> 7ccbec8 (feat(array):add Assignment3)
+<<<<<<< HEAD
 =======
  * @author H071171512 - Fitrah Muhammad <fitrahm17h@student.unhas.ac.id>
 >>>>>>> d53c340 (feat(array): add assignment 3 placeholder files)
+=======
+>>>>>>> 08a8b6c (feat(array):add Assignment3)
  *
  */
 public class RadixConverter {
